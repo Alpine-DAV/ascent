1d4458df855b3ead0ad29deb008a4eba018db593
Matt Larsen <larsen30@llnl.gov>
2017-06-05 15:08:11 -0700

    adding missing file

--- src/vtk-h/vtkh_renderer.hpp
+++ src/vtk-h/vtkh_renderer.hpp
@@ -2,19 +2,29 @@
 #define VTK_H_RENDERER_HPP
 
 #include <vector>
-#include <vtkhFilter.hpp>
+#include <vtkh_filter.hpp>
+
+#include <vtkm/rendering/Camera.h>
+#include <vtkm/rendering/Canvas.h>
+
+namespace vtkh {
 
 class vtkhRenderer : public vtkhFilter
 {
 protected:
-  typedef vtkm::rendering::Canvas vtkmCanvas; 
+  
+  // image related data with cinema support
+  typedef std::shared_ptr<vtkm::rendering::Canvas> vtkmCanvasPtr; 
   typedef vtkm::rendering::Camera vtkmCamera; 
-  std::vector<vtkmCanvas *> m_canvases;
-  std::vector<vtkmCamera>   m_cameras;
-  int                       m_batch_size;
-   
+  std::vector<vtkmCanvasPtr> m_canvases;
+  std::vector<vtkmCamera>    m_cameras;
+  int                        m_batch_size;
+  // draw annoations?? 
+  bool                       m_world_annotations;   
+  bool                       m_screen_annotations;   
 public:
   
 };
 
+} // namespace vtkh
 #endif
