0c5e6ed38ef024c9f4048b4228f4a4cae9b468fe
Cyrus Harrison <cyrush@llnl.gov>
2018-06-22 16:58:55 -0700

    travis:use more external pkgs

--- scripts/uberenv/uberenv.py
+++ scripts/uberenv/uberenv.py
@@ -376,6 +376,11 @@ def main():
     # twist spack's arms 
     cfg_dir = uberenv_spack_config_dir(opts, uberenv_path)
     patch_spack(dest_spack, uberenv_path, cfg_dir, pkgs)
+    
+    # show the speck for what will be built
+    spec_cmd = "spack/bin/spack spec " + uberenv_pkg_name + opts["spec"]
+    res = sexe(spec_cmd, echo=True)
+
 
     ##########################################################
     # we now have an instance of spack configured how we 
