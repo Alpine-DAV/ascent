f008218b95414835b4c2d94653820e279179eb88
Matt Larsen <mclarsen@users.noreply.github.com>
2018-12-02 08:38:14 -0800

    Fix/hola (#188)
    * fixing indexing error with serial hola, and fighting the war against trailing whitespace
    
    * adding distributed hola relay::mpi domain reads
    
    * adding multi time step ability to replay

--- src/ascent/hola/ascent_hola.cpp
+++ src/ascent/hola/ascent_hola.cpp
@@ -1,45 +1,45 @@
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 // Copyright (c) 2015-2018, Lawrence Livermore National Security, LLC.
-// 
+//
 // Produced at the Lawrence Livermore National Laboratory
-// 
+//
 // LLNL-CODE-716457
-// 
+//
 // All rights reserved.
-// 
-// This file is part of Ascent. 
-// 
+//
+// This file is part of Ascent.
+//
 // For details, see: http://ascent.readthedocs.io/.
-// 
+//
 // Please also read ascent/LICENSE
-// 
-// Redistribution and use in source and binary forms, with or without 
+//
+// Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
-// 
-// * Redistributions of source code must retain the above copyright notice, 
+//
+// * Redistributions of source code must retain the above copyright notice,
 //   this list of conditions and the disclaimer below.
-// 
+//
 // * Redistributions in binary form must reproduce the above copyright notice,
 //   this list of conditions and the disclaimer (as noted below) in the
 //   documentation and/or other materials provided with the distribution.
-// 
+//
 // * Neither the name of the LLNS/LLNL nor the names of its contributors may
 //   be used to endorse or promote products derived from this software without
 //   specific prior written permission.
-// 
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 // AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 // IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
 // ARE DISCLAIMED. IN NO EVENT SHALL LAWRENCE LIVERMORE NATIONAL SECURITY,
 // LLC, THE U.S. DEPARTMENT OF ENERGY OR CONTRIBUTORS BE LIABLE FOR ANY
-// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
 // DAMAGES  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
 // OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
 // STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
-// IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+// IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
-// 
+//
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 
 
@@ -66,6 +66,7 @@
 
 #if defined(ASCENT_MPI_ENABLED)
     #include "ascent_hola_mpi.hpp"
+    #include <conduit_relay_mpi.hpp>
 #endif
 
 using namespace conduit;
@@ -95,13 +96,13 @@ public:
       m_protocol(protocol),
       m_mesh_index(mesh_index)
     {
-        
+
     }
-   
+
     //-------------------------------------------------------------------//
     ~BlueprintTreePathGenerator()
     {
-        
+
     }
 
     //-------------------------------------------------------------------//
@@ -182,11 +183,11 @@ private:
 //-----------------------------------------------------------------------------
 void relay_blueprint_mesh_read(const Node &options,
                                Node &data)
-{    
+{
     std::string root_fname = options["root_file"].as_string();
-    
+
     // read the root file, it can be either json or hdf5
-    
+
     // assume hdf5, but check for json file
     std::string root_protocol = "hdf5";
     char buff[5] = {0,0,0,0,0};
@@ -200,14 +201,14 @@ void relay_blueprint_mesh_read(const Node &options,
     }
     ifs.read((char *)buff,5);
     ifs.close();
-            
+
     std::string test_str(buff);
 
     if(test_str.find("{") != std::string::npos)
     {
        root_protocol = "json";
     }
-    
+
     Node root_node;
     relay::io::load(root_fname, root_protocol, root_node);
 
@@ -216,22 +217,22 @@ void relay_blueprint_mesh_read(const Node &options,
     {
         ASCENT_ERROR("Root file missing 'file_pattern'");
     }
-        
+
     if(!root_node.has_child("blueprint_index"))
     {
         ASCENT_ERROR("Root file missing 'blueprint_index'");
     }
-        
+
     NodeConstIterator itr =root_node["blueprint_index"].children();
     Node verify_info;
     // TODO, for now lets verify the first mesh index
-    
+
     const Node &mesh_index = itr.next();
 
     if( !blueprint::mesh::index::verify(mesh_index,
                                         verify_info[itr.name()]))
     {
-        ASCENT_ERROR("Mesh Blueprint index verify failed" << std::endl 
+        ASCENT_ERROR("Mesh Blueprint index verify failed" << std::endl
                      << verify_info.to_json());
     }
 
@@ -241,29 +242,72 @@ void relay_blueprint_mesh_read(const Node &options,
     {
         data_protocol = root_node["protocol/name"].as_string();
     }
-    
+
     // read the first mesh (all domains ...)
-    
+
     int num_domains = root_node["number_of_trees"].to_int();
-    
+
     BlueprintTreePathGenerator gen(root_node["file_pattern"].as_string(),
                                    root_node["tree_pattern"].as_string(),
                                    root_node["number_of_files"].to_int(),
                                    num_domains,
                                    data_protocol,
                                    mesh_index);
-    
+
     std::ostringstream oss;
-    
-    // TODO: distrib over mpi tasks when MPI
-    
-    for(int i=0;i<num_domains;i++)
+
+    int domain_start = 0;
+    int domain_end = num_domains;
+
+#if defined(ASCENT_MPI_ENABLED)
+    MPI_Comm comm  = MPI_Comm_f2c(options["mpi_comm"].to_int());
+    int rank = relay::mpi::rank(comm);
+    int total_size = relay::mpi::size(comm);
+
+    if(num_domains < total_size)
+    {
+      if(rank == 0)
+      {
+        ASCENT_ERROR("hola: total domains "<<num_domains<<" must be equal to "
+                     <<"or greater than the number of ranks "<<total_size<<".");
+      }
+    }
+
+    int read_size = num_domains / total_size;
+    int rem = num_domains % total_size;
+    if(rank < rem)
+    {
+      read_size++;
+    }
+
+    conduit::Node n_read_size;
+    conduit::Node n_doms_per_rank;
+
+    n_read_size.set_int32(read_size);
+
+    relay::mpi::all_gather_using_schema(n_read_size,
+                                        n_doms_per_rank,
+                                        comm);
+    int *counts = (int*)n_doms_per_rank.data_ptr();
+
+    int rank_offset = 0;
+    for(int i = 0; i < rank; ++i)
+    {
+      rank_offset += counts[i];
+    }
+
+    domain_start = rank_offset;
+    domain_end = rank_offset + read_size;
+#endif
+
+
+    for(int i = domain_start ; i < domain_end; i++)
     {
         char domain_fmt_buff[64];
         snprintf(domain_fmt_buff, sizeof(domain_fmt_buff), "%06d",i);
         oss.str("");
         oss << "domain_" << std::string(domain_fmt_buff);
-        relay::io::load(gen.GenerateFilePath(0),
+        relay::io::load(gen.GenerateFilePath(i),
                         data_protocol,
                         data[oss.str()]);
     }
@@ -283,7 +327,7 @@ void hola(const std::string &source,
     {
 #if defined(ASCENT_MPI_ENABLED)
         hola_mpi(options,data);
-#else 
+#else
         ASCENT_ERROR("mpi disabled: 'hola_mpi' can only be used in ascent_mpi" );
 #endif
     }
--- src/utilities/replay/replay.cpp
+++ src/utilities/replay/replay.cpp
@@ -1,45 +1,45 @@
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 // Copyright (c) 2015-2018, Lawrence Livermore National Security, LLC.
-// 
+//
 // Produced at the Lawrence Livermore National Laboratory
-// 
+//
 // LLNL-CODE-716457
-// 
+//
 // All rights reserved.
-// 
-// This file is part of Ascent. 
-// 
+//
+// This file is part of Ascent.
+//
 // For details, see: http://ascent.readthedocs.io/.
-// 
+//
 // Please also read ascent/LICENSE
-// 
-// Redistribution and use in source and binary forms, with or without 
+//
+// Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
-// 
-// * Redistributions of source code must retain the above copyright notice, 
+//
+// * Redistributions of source code must retain the above copyright notice,
 //   this list of conditions and the disclaimer below.
-// 
+//
 // * Redistributions in binary form must reproduce the above copyright notice,
 //   this list of conditions and the disclaimer (as noted below) in the
 //   documentation and/or other materials provided with the distribution.
-// 
+//
 // * Neither the name of the LLNS/LLNL nor the names of its contributors may
 //   be used to endorse or promote products derived from this software without
 //   specific prior written permission.
-// 
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 // AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 // IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
 // ARE DISCLAIMED. IN NO EVENT SHALL LAWRENCE LIVERMORE NATIONAL SECURITY,
 // LLC, THE U.S. DEPARTMENT OF ENERGY OR CONTRIBUTORS BE LIABLE FOR ANY
-// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
 // DAMAGES  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
 // OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
 // STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
-// IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+// IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
-// 
+//
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 
 
@@ -51,15 +51,36 @@
 #include <ascent.hpp>
 #include <ascent_hola.hpp>
 
+#include <fstream>
 #include <vector>
 #ifdef REPLAY_MPI
 #include <mpi.h>
 #endif
 
+void usage()
+{
+  std::cout<<"replay usage:\n";
+  std::cout<<"replay is a utility for 'replaying' the data files saved during the course ";
+  std::cout<<"of a simulation. Domain overloading is supported, so you can load x domains ";
+  std::cout<<"with y mpi ranks where x >= y\n\n.";
+  std::cout<<"======================== Options  =========================\n";
+  std::cout<<"  --root    : the root file for a blueprint hdf5 set of files.\n";
+  std::cout<<"  --cycles  : a text file containing a list of root files, one per line.\n";
+  std::cout<<"              Each file will be loaded and sent to Ascent in order.\n";
+  std::cout<<"  --actions : a json file containing ascent actions. Default value\n";
+  std::cout<<"              is 'ascent_actions.json'.\n\n";
+  std::cout<<"======================== Examples =========================\n";
+  std::cout<<"./relay_ser --root=clover.cycle_000060.root\n";
+  std::cout<<"./relay_ser --root=clover.cycle_000060.root --actions=my_actions.json\n";
+  std::cout<<"srun -n 4 relay_mpi --cycles=cycles_file\n";
+  std::cout<<"\n\n";
+}
+
 struct Options
 {
   std::string m_actions_file = "ascent_actions.json";
   std::string m_root_file;
+  std::string m_cycles_file;
 
   void parse(int argc, char** argv)
   {
@@ -67,44 +88,55 @@ struct Options
     {
       if(contains(argv[i], "--root="))
       {
-        m_root_file = get_arg(argv[i]); 
+        m_root_file = get_arg(argv[i]);
+      }
+      if(contains(argv[i], "--cycles="))
+      {
+        m_cycles_file = get_arg(argv[i]);
       }
       else if(contains(argv[i], "--actions="))
       {
-        m_actions_file = get_arg(argv[i]); 
+        m_actions_file = get_arg(argv[i]);
       }
       else
       {
-        usage(argv[i]);
+        bad_arg(argv[i]);
       }
     }
-    if(m_root_file == "")
+    if(m_root_file == "" && m_cycles_file == "")
     {
-      std::cerr<<"Required option '--root' not specified. Bailing...\n";
+      std::cerr<<"You must specify a '--root' or '--cycles' files. Bailing...\n";
+      usage();
+      exit(1);
+    }
+    if(m_root_file != "" && m_cycles_file != "")
+    {
+      std::cerr<<"You must specify either '--root' or '--cycles' files but not both. Bailing...\n";
+      usage();
       exit(1);
     }
   }
 
-	std::vector<std::string> &split(const std::string &s, 
-                                  char delim, 
-                                  std::vector<std::string> &elems)
-	{   
-		std::stringstream ss(s);
-		std::string item;
+std::vector<std::string> &split(const std::string &s,
+                                char delim,
+                                std::vector<std::string> &elems)
+{
+  std::stringstream ss(s);
+  std::string item;
 
-		while (std::getline(ss, item, delim))
-		{   
-			 elems.push_back(item);
-		}
-		return elems;
+  while (std::getline(ss, item, delim))
+  {
+   elems.push_back(item);
   }
-	 
-	std::vector<std::string> split(const std::string &s, char delim)
-	{   
-		std::vector<std::string> elems;
-		split(s, delim, elems);
-		return elems;
-	} 
+  return elems;
+}
+
+std::vector<std::string> split(const std::string &s, char delim)
+{
+  std::vector<std::string> elems;
+  split(s, delim, elems);
+  return elems;
+}
   std::string get_arg(const char *arg)
   {
     std::vector<std::string> parse;
@@ -115,27 +147,25 @@ struct Options
 
     if(parse.size() != 2)
     {
-      usage(arg);
+      bad_arg(arg);
     }
     else
     {
       res = parse[1];
-    } 
+    }
     return res;
   }
 
-	bool contains(const std::string haystack, std::string needle)
-	{
-		std::size_t found = haystack.find(needle);
-		return (found != std::string::npos);
-	}
+bool contains(const std::string haystack, std::string needle)
+{
+  std::size_t found = haystack.find(needle);
+  return (found != std::string::npos);
+}
 
-  void usage(std::string bad_arg)
+  void bad_arg(std::string bad_arg)
   {
     std::cerr<<"Invalid argument \""<<bad_arg<<"\"\n";
-    std::cout<<"Replay usage: "
-             <<"       --root    : blueprint rootfile\n"
-             <<"       --actions : ascent_actions.json file\n";
+    usage();
     exit(0);
   }
 };
@@ -144,6 +174,23 @@ int main (int argc, char *argv[])
 {
   Options options;
   options.parse(argc, argv);
+
+  std::vector<std::string> time_steps;
+
+  if(options.m_root_file != "")
+  {
+    time_steps.push_back(options.m_root_file);
+  }
+  else
+  {
+
+    std::ifstream in_file(options.m_cycles_file);
+    std::string line;
+    while(!getline(in_file, line).eof())
+    {
+      time_steps.push_back(line);
+    }
+  }
 
   int comm_size = 1;
   int rank = 0;
@@ -155,11 +202,12 @@ int main (int argc, char *argv[])
 #endif
 
   conduit::Node replay_data, replay_opts;
-  replay_opts["root_file"] = options.m_root_file;
-  ascent::hola("relay/blueprint/mesh", replay_opts, replay_data);
-  //replay_data.print(); 
+#ifdef REPLAY_MPI
+  replay_opts["mpi_comm"] = MPI_Comm_c2f(MPI_COMM_WORLD);
+#endif
+  //replay_data.print();
   conduit::Node ascent_opts;
-  ascent_opts["actions_file"] = options.m_actions_file; 
+  ascent_opts["actions_file"] = options.m_actions_file;
   ascent_opts["ascent_info"] = "verbose";
 #ifdef REPLAY_MPI
   ascent_opts["mpi_comm"] = MPI_Comm_c2f(MPI_COMM_WORLD);
@@ -168,13 +216,23 @@ int main (int argc, char *argv[])
   //
   // Blank actions to be populated from actions file
   //
-  conduit::Node actions; 
+  conduit::Node actions;
 
   ascent::Ascent ascent;
   ascent.open(ascent_opts);
-  ascent.publish(replay_data);
-  ascent.execute(actions);
+
+  for(int i = 0; i < time_steps.size(); ++i)
+  {
+    replay_opts["root_file"] = time_steps[i];
+    ascent::hola("relay/blueprint/mesh", replay_opts, replay_data);
+
+    ascent.publish(replay_data);
+    ascent.execute(actions);
+  }
   ascent.close();
 
+#ifdef REPLAY_MPI
+  MPI_Finalize();
+#endif
   return 0;
 }
