0243ad6aabd085ff6b25e21beb5f30aec32d7b5a
mclarsen <larsen30@llnl.gov>
2018-12-01 17:53:33 -0800

    adding multi time step ability to replay

--- src/utilities/replay/replay.cpp
+++ src/utilities/replay/replay.cpp
@@ -51,15 +51,36 @@
 #include <ascent.hpp>
 #include <ascent_hola.hpp>
 
+#include <fstream>
 #include <vector>
 #ifdef REPLAY_MPI
 #include <mpi.h>
 #endif
 
+void usage()
+{
+  std::cout<<"replay usage:\n";
+  std::cout<<"replay is a utility for 'replaying' the data files saved during the course ";
+  std::cout<<"of a simulation. Domain overloading is supported, so you can load x domains ";
+  std::cout<<"with y mpi ranks where x >= y\n\n.";
+  std::cout<<"======================== Options  =========================\n";
+  std::cout<<"  --root    : the root file for a blueprint hdf5 set of files.\n";
+  std::cout<<"  --cycles  : a text file containing a list of root files, one per line.\n";
+  std::cout<<"              Each file will be loaded and sent to Ascent in order.\n";
+  std::cout<<"  --actions : a json file containing ascent actions. Default value\n";
+  std::cout<<"              is 'ascent_actions.json'.\n\n";
+  std::cout<<"======================== Examples =========================\n";
+  std::cout<<"./relay_ser --root=clover.cycle_000060.root\n";
+  std::cout<<"./relay_ser --root=clover.cycle_000060.root --actions=my_actions.json\n";
+  std::cout<<"srun -n 4 relay_mpi --cycles=cycles_file\n";
+  std::cout<<"\n\n";
+}
+
 struct Options
 {
   std::string m_actions_file = "ascent_actions.json";
   std::string m_root_file;
+  std::string m_cycles_file;
 
   void parse(int argc, char** argv)
   {
@@ -69,42 +90,53 @@ struct Options
       {
         m_root_file = get_arg(argv[i]);
       }
+      if(contains(argv[i], "--cycles="))
+      {
+        m_cycles_file = get_arg(argv[i]);
+      }
       else if(contains(argv[i], "--actions="))
       {
         m_actions_file = get_arg(argv[i]);
       }
       else
       {
-        usage(argv[i]);
+        bad_arg(argv[i]);
       }
     }
-    if(m_root_file == "")
+    if(m_root_file == "" && m_cycles_file == "")
     {
-      std::cerr<<"Required option '--root' not specified. Bailing...\n";
+      std::cerr<<"You must specify a '--root' or '--cycles' files. Bailing...\n";
+      usage();
+      exit(1);
+    }
+    if(m_root_file != "" && m_cycles_file != "")
+    {
+      std::cerr<<"You must specify either '--root' or '--cycles' files but not both. Bailing...\n";
+      usage();
       exit(1);
     }
   }
 
-	std::vector<std::string> &split(const std::string &s,
-                                  char delim,
-                                  std::vector<std::string> &elems)
-	{
-		std::stringstream ss(s);
-		std::string item;
+std::vector<std::string> &split(const std::string &s,
+                                char delim,
+                                std::vector<std::string> &elems)
+{
+  std::stringstream ss(s);
+  std::string item;
 
-		while (std::getline(ss, item, delim))
-		{
-			 elems.push_back(item);
-		}
-		return elems;
+  while (std::getline(ss, item, delim))
+  {
+   elems.push_back(item);
   }
+  return elems;
+}
 
-	std::vector<std::string> split(const std::string &s, char delim)
-	{
-		std::vector<std::string> elems;
-		split(s, delim, elems);
-		return elems;
-	}
+std::vector<std::string> split(const std::string &s, char delim)
+{
+  std::vector<std::string> elems;
+  split(s, delim, elems);
+  return elems;
+}
   std::string get_arg(const char *arg)
   {
     std::vector<std::string> parse;
@@ -115,7 +147,7 @@ struct Options
 
     if(parse.size() != 2)
     {
-      usage(arg);
+      bad_arg(arg);
     }
     else
     {
@@ -124,18 +156,16 @@ struct Options
     return res;
   }
 
-	bool contains(const std::string haystack, std::string needle)
-	{
-		std::size_t found = haystack.find(needle);
-		return (found != std::string::npos);
-	}
+bool contains(const std::string haystack, std::string needle)
+{
+  std::size_t found = haystack.find(needle);
+  return (found != std::string::npos);
+}
 
-  void usage(std::string bad_arg)
+  void bad_arg(std::string bad_arg)
   {
     std::cerr<<"Invalid argument \""<<bad_arg<<"\"\n";
-    std::cout<<"Replay usage: "
-             <<"       --root    : blueprint rootfile\n"
-             <<"       --actions : ascent_actions.json file\n";
+    usage();
     exit(0);
   }
 };
@@ -144,6 +174,23 @@ int main (int argc, char *argv[])
 {
   Options options;
   options.parse(argc, argv);
+
+  std::vector<std::string> time_steps;
+
+  if(options.m_root_file != "")
+  {
+    time_steps.push_back(options.m_root_file);
+  }
+  else
+  {
+
+    std::ifstream in_file(options.m_cycles_file);
+    std::string line;
+    while(!getline(in_file, line).eof())
+    {
+      time_steps.push_back(line);
+    }
+  }
 
   int comm_size = 1;
   int rank = 0;
@@ -158,8 +205,6 @@ int main (int argc, char *argv[])
 #ifdef REPLAY_MPI
   replay_opts["mpi_comm"] = MPI_Comm_c2f(MPI_COMM_WORLD);
 #endif
-  replay_opts["root_file"] = options.m_root_file;
-  ascent::hola("relay/blueprint/mesh", replay_opts, replay_data);
   //replay_data.print();
   conduit::Node ascent_opts;
   ascent_opts["actions_file"] = options.m_actions_file;
@@ -175,8 +220,15 @@ int main (int argc, char *argv[])
 
   ascent::Ascent ascent;
   ascent.open(ascent_opts);
-  ascent.publish(replay_data);
-  ascent.execute(actions);
+
+  for(int i = 0; i < time_steps.size(); ++i)
+  {
+    replay_opts["root_file"] = time_steps[i];
+    ascent::hola("relay/blueprint/mesh", replay_opts, replay_data);
+
+    ascent.publish(replay_data);
+    ascent.execute(actions);
+  }
   ascent.close();
 
 #ifdef REPLAY_MPI
