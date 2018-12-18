6c2d25e149b6720c6a9da6f484be6007e1e3e6d6
mclarsen <larsen30@llnl.gov>
2018-12-18 13:40:27 -0800

    fixing lagrange test

--- src/tests/ascent/t_ascent_lagrangian.cpp
+++ src/tests/ascent/t_ascent_lagrangian.cpp
@@ -1,45 +1,45 @@
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 // Copyright (c) 2015-2018, Lawrence Livermore National Security, LLC.
-// 
+//
 // Produced at the Lawrence Livermore National Laboratory
-// 
+//
 // LLNL-CODE-716457
-// 
+//
 // All rights reserved.
-// 
-// This file is part of Ascent. 
-// 
+//
+// This file is part of Ascent.
+//
 // For details, see: http://ascent.readthedocs.io/.
-// 
+//
 // Please also read ascent/LICENSE
-// 
-// Redistribution and use in source and binary forms, with or without 
+//
+// Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
-// 
-// * Redistributions of source code must retain the above copyright notice, 
+//
+// * Redistributions of source code must retain the above copyright notice,
 //   this list of conditions and the disclaimer below.
-// 
+//
 // * Redistributions in binary form must reproduce the above copyright notice,
 //   this list of conditions and the disclaimer (as noted below) in the
 //   documentation and/or other materials provided with the distribution.
-// 
+//
 // * Neither the name of the LLNS/LLNL nor the names of its contributors may
 //   be used to endorse or promote products derived from this software without
 //   specific prior written permission.
-// 
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 // AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 // IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
 // ARE DISCLAIMED. IN NO EVENT SHALL LAWRENCE LIVERMORE NATIONAL SECURITY,
 // LLC, THE U.S. DEPARTMENT OF ENERGY OR CONTRIBUTORS BE LIABLE FOR ANY
-// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
 // DAMAGES  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
 // OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
 // STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
-// IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+// IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
-// 
+//
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
 
 //-----------------------------------------------------------------------------
@@ -81,27 +81,27 @@ TEST(ascent_lagrangian, test_lagrangian_multistep)
         ASCENT_INFO("Ascent vtkm support disabled, skipping test");
         return;
     }
-    
+
     ASCENT_INFO("Testing lagrangian");
-    
+
 
 //    string output_path = prepare_output_dir();
 //    string output_file = conduit::utils::join_file_path(output_path,"tout_lagrangian_3d");
-   
+
     string output_path = ASCENT_T_BIN_DIR;
-    
+
     ASCENT_INFO("Execute test from folder: " + output_path + "/ascent");
     output_path = conduit::utils::join_file_path(output_path,"ascent/output");
     ASCENT_INFO("Creating output folder: " + output_path);
     if(!conduit::utils::is_directory(output_path))
-    {   
+    {
         conduit::utils::create_directory(output_path);
-    } 
- 
+    }
+
     // remove old images before rendering
-    string output_file1 = conduit::utils::join_file_path(output_path,"basisflows_1_5.vtk");
+    string output_file1 = conduit::utils::join_file_path(output_path,"basisflows_0_5.vtk");
     remove_test_file(output_file1);
-    string output_file2 = conduit::utils::join_file_path(output_path,"basisflows_1_10.vtk");
+    string output_file2 = conduit::utils::join_file_path(output_path,"basisflows_0_10.vtk");
     remove_test_file(output_file2);
 
     ASCENT_INFO(output_file1);
@@ -110,7 +110,7 @@ TEST(ascent_lagrangian, test_lagrangian_multistep)
     //
     // Create the actions.
     //
-    
+
     conduit::Node pipelines;
     // pipeline 1
     pipelines["pl1/f1/type"] = "lagrangian";
@@ -135,17 +135,17 @@ TEST(ascent_lagrangian, test_lagrangian_multistep)
     // reset
     conduit::Node &reset  = actions.append();
     reset["action"] = "reset";
-    
+
     //
     // Run Ascent
     //
-    
+
     Ascent ascent;
 
     Node ascent_opts;
     ascent_opts["runtime/type"] = "ascent";
     ascent.open(ascent_opts);
-    
+
     for(int cycle = 1; cycle <= 10; cycle++)
     {
       //
@@ -157,19 +157,19 @@ TEST(ascent_lagrangian, test_lagrangian_multistep)
                                               EXAMPLE_MESH_SIDE_DIM,
                                               EXAMPLE_MESH_SIDE_DIM,
                                               data);
-    
+
       EXPECT_TRUE(conduit::blueprint::mesh::verify(data,verify_info));
       ascent.publish(data);
       ascent.execute(actions);
     }
-    
+
     ascent.close();
-    
+
     // check that we created the right output
     EXPECT_TRUE(check_test_file(output_file1));
     EXPECT_TRUE(check_test_file(output_file2));
 
-    // clean up 
+    // clean up
     remove_test_file(output_file1);
     remove_test_file(output_file2);
     conduit::utils::remove_directory(output_path);
@@ -180,13 +180,13 @@ int main(int argc, char* argv[])
     int result = 0;
 
     ::testing::InitGoogleTest(&argc, argv);
-    
+
     // allow override of the data size via the command line
     if(argc == 2)
-    { 
+    {
         EXAMPLE_MESH_SIDE_DIM = atoi(argv[1]);
     }
-    
+
     result = RUN_ALL_TESTS();
     return result;
 }
