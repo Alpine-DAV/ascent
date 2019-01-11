b1b613d9b1c79e96e17de772942df544eb20687d
Cyrus Harrison <cyrush@llnl.gov>
2019-01-11 14:56:22 -0800

    add cori nersc install script

---
+++ scripts/spack_install/nersc_cori_intel.sh
@@ -0,0 +1,3 @@
+python uberenv/uberenv.py --install --run_tests --pull  --spec="%intel~python~fortran~test ^conduit~fortran"  --spack-config-dir="uberenv/spack_configs/nersc/cori/" --prefix="/project/projectdirs/alpine/software/ascent/ecp_2019/"
+
+
