23b350c6611ac2ec98dfe90d55e73ebbfff2fc27
Matt Larsen <mclarsen@users.noreply.github.com>
2019-03-18 10:52:57 -0700

    Task/image diff (#270)
    * cleanup clang warnings
    
    * image compare
    
    * revert slice
    
    * adding image check in tests
    
    * adding diff unit test
    
    * adding depends on ascent because of utils
    
    * lowering image tolerance for meshes which seem to be non-deterministic
    
    * further decreasing mesh plot diff tolerance

--- src/ascent/runtimes/ascent_main_runtime.hpp
+++ src/ascent/runtimes/ascent_main_runtime.hpp
@@ -74,14 +74,14 @@ public:
     virtual ~AscentRuntime();
 
     // Main runtime interface methods used by the ascent interface.
-    void  Initialize(const conduit::Node &options);
+    void  Initialize(const conduit::Node &options) override;
 
-    void  Publish(const conduit::Node &data);
-    void  Execute(const conduit::Node &actions);
+    void  Publish(const conduit::Node &data) override;
+    void  Execute(const conduit::Node &actions) override;
 
-    void  Info(conduit::Node &out);
+    void  Info(conduit::Node &out) override;
 
-    void  Cleanup();
+    void  Cleanup() override;
 
     void DisplayError(const std::string &msg) override;
 
---
+++ src/ascent/utils/ascent_png_compare.hpp
@@ -0,0 +1,90 @@
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+// Copyright (c) 2015-2019, Lawrence Livermore National Security, LLC.
+//
+// Produced at the Lawrence Livermore National Laboratory
+//
+// LLNL-CODE-716457
+//
+// All rights reserved.
+//
+// This file is part of Ascent.
+//
+// For details, see: http://ascent.readthedocs.io/.
+//
+// Please also read ascent/LICENSE
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the disclaimer below.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the disclaimer (as noted below) in the
+//   documentation and/or other materials provided with the distribution.
+//
+// * Neither the name of the LLNS/LLNL nor the names of its contributors may
+//   be used to endorse or promote products derived from this software without
+//   specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL LAWRENCE LIVERMORE NATIONAL SECURITY,
+// LLC, THE U.S. DEPARTMENT OF ENERGY OR CONTRIBUTORS BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+// OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+// IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+//-----------------------------------------------------------------------------
+///
+/// file: ascent_png_compare.hpp
+///
+//-----------------------------------------------------------------------------
+#ifndef ASCENT_PNG_COMPARE_HPP
+#define ASCENT_PNG_COMPARE_HPP
+
+#include <string>
+#include <conduit.hpp>
+
+//-----------------------------------------------------------------------------
+// -- begin ascent:: --
+//-----------------------------------------------------------------------------
+namespace ascent
+{
+
+class PNGCompare
+{
+public:
+    PNGCompare();
+    ~PNGCompare();
+    bool Compare(const std::string &img1,
+                 const std::string &img2,
+                 conduit::Node &info,
+                 const float tolarance = 0.001f);
+private:
+    void DiffImage(const unsigned char *buff_1,
+                   const unsigned char *buff_2,
+                   const int width,
+                   const int height,
+                   const std::string out_name);
+};
+
+//-----------------------------------------------------------------------------
+};
+//-----------------------------------------------------------------------------
+// -- end ascent:: --
+//-----------------------------------------------------------------------------
+
+#endif
+//-----------------------------------------------------------------------------
+// -- end header ifdef guard
+//-----------------------------------------------------------------------------
+
+
