392115920c47bb5379e2983d99e811d4b8d8cab5
Cyrus Harrison <cyrush@llnl.gov>
2019-05-29 05:43:10 -0700

    updates to cori spack configs

---
+++ scripts/spack_install/nersc_cori_gcc_7.3.0.sh
@@ -0,0 +1,3 @@
+python uberenv/uberenv.py --install --pull  --spec="%gcc"  --spack-config-dir="uberenv/spack_configs/nersc/cori/" --prefix="/project/projectdirs/alpine/software/ascent/gnu/7.3.0"
+
+
