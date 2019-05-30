24e5d15422c6526dfc5f1397deb566c7f0a5dccd
Cyrus Harrison <cyrush@llnl.gov>
2019-05-30 11:07:00 -0600

    nersc cori build updates (#288)
    * updates to cori spack configs
    
    * updates for cori

--- scripts/spack_install/nersc_cori_intel.sh
+++
@@ -1,3 +0,0 @@
-python uberenv/uberenv.py --install --run_tests --pull  --spec="%intel~python~fortran~test ^conduit~fortran"  --spack-config-dir="uberenv/spack_configs/nersc/cori/" --prefix="/project/projectdirs/alpine/software/ascent/ecp_2019/"
-
-
