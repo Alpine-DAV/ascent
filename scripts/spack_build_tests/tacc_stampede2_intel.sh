973bfe9d338faa9be16b0a397798fb311a869918
Cyrus Harrison <cyrush@llnl.gov>
2019-05-30 14:16:55 -0500

    more tacc build script updates

--- scripts/spack_build_tests/tacc_stampede2_intel.sh
+++ scripts/spack_build_tests/tacc_stampede2_intel.sh
@@ -1,3 +1,3 @@
-python uberenv/uberenv.py --install --pull --spec="%intel~python" --spack-config-dir=uberenv/spack_configs/tacc/stampede2/ --prefix="_uberenv_test_tacc_gcc"
+python uberenv/uberenv.py --install --pull --spec="%intel" --spack-config-dir=uberenv/spack_configs/tacc/stampede2/ --prefix="_uberenv_test_tacc_gcc"
 
 
