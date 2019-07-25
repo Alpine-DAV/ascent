a16a583774a706263a0b32d729c007dad3487dc7
larsen30@llnl.gov <larsen30@rzansel61.coral.llnl.gov>
2019-07-25 13:12:37 -0700

    updating hashes

--- scripts/uberenv/packages/vtkh/package.py
+++ scripts/uberenv/packages/vtkh/package.py
@@ -38,7 +38,7 @@ class Vtkh(Package):
     maintainers = ['cyrush']
 
 
-    version('ascent_ver', commit='b6e3acc801917436b27df3fb4841daffd5588d45', submodules=True, preferred=True)
+    version('ascent_ver', commit='d868120c2594754569ecfb10fb1a97a490fc9155', submodules=True, preferred=True)
     version('develop', branch='develop', submodules=True)
     version('0.1.0', branch='develop', tag='v0.1.0', submodules=True)
 
--- scripts/uberenv/packages/vtkm/package.py
+++ scripts/uberenv/packages/vtkm/package.py
@@ -21,7 +21,7 @@ class Vtkm(CMakePackage, CudaPackage):
     git      = "https://gitlab.kitware.com/vtk/vtk-m.git"
 
     # version used for ascent
-    version('ascent_ver', commit='c80c1d09c043b9af04839a92cd46b12ac673fc98', preferred=True)
+    version('ascent_ver', commit='403bca94c7166e9428119c6d1cc8d40f81583faf', preferred=True)
     version('master', branch='master')
     version('1.3.0', "d9f6e274dec2ea01273cccaba356d23ca88c5a25")
     version('1.2.0', "3295fed86012226c107e1f2605ca7cc583586b63")
