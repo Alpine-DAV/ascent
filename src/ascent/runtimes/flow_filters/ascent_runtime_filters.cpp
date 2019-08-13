76faddcfce892a79a83e602dd6d4c1ad0d74f646
Matt Larsen <larsen30@llnl.gov>
2019-08-13 07:43:01 -0700

    exposing recenter

--- src/ascent/runtimes/flow_filters/ascent_runtime_filters.cpp
+++ src/ascent/runtimes/flow_filters/ascent_runtime_filters.cpp
@@ -136,6 +136,7 @@ register_builtin()
     AscentRuntime::register_filter_type<VTKHSlice>("transforms","slice");
     AscentRuntime::register_filter_type<VTKH3Slice>("transforms","3slice");
     AscentRuntime::register_filter_type<VTKHNoOp>("transforms","noop");
+    AscentRuntime::register_filter_type<VTKHRecenter>("transforms","recenter");
     AscentRuntime::register_filter_type<VTKHVectorMagnitude>("transforms","vector_magnitude");
     AscentRuntime::register_filter_type<RoverXRay>("extracts", "xray");
     AscentRuntime::register_filter_type<RoverVolume>("extracts", "volume");
