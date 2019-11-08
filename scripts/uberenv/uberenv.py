4988da726b4458277b403d9422fea0ef09bad1ff
Cyrus Harrison <cyrush@llnl.gov>
2019-11-08 11:34:44 -0800

    uberenv activate fix for new spack

--- scripts/uberenv/uberenv.py
+++ scripts/uberenv/uberenv.py
@@ -356,8 +356,13 @@ def setup_osx_sdk_env_vars():
 def find_spack_pkg_path(pkg_name):
     r,rout = sexe("spack/bin/spack find -p " + pkg_name,ret_output = True)
     for l in rout.split("\n"):
-        if l.startswith(" "):
+        lstrip = l.strip()
+        if not lstrip == "" and \
+           not lstrip.startswith("==>") and  \
+           not lstrip.startswith("--"):
             return {"name": pkg_name, "path": l.split()[-1]}
+    print("[ERROR: failed to find package named '{}']".format(pkg_name))
+    sys.exit(-1)
 
 def read_spack_full_spec(pkg_name,spec):
     rv, res = sexe("spack/bin/spack spec " + pkg_name + " " + spec, ret_output=True)
