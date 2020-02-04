67c6dd7402ac049f49515eb0a4998e660b1ec33c
Cyrus Harrison <cyrush@llnl.gov>
2020-02-03 16:19:32 -0800

    fix uber/spack sha for 0.5.1 rel tarball (#455)

--- scripts/uberenv/packages/ascent/package.py
+++ scripts/uberenv/packages/ascent/package.py
@@ -43,7 +43,7 @@ class Ascent(Package, CudaPackage):
             branch='develop',
             submodules=True)
 
-    version('0.5.1', sha256='2adf3daa3d3eea22e634f32207bb8cfe1e2fbe2c9013c47c70c6a3956ad88e19')
+    version('0.5.1', sha256='6ad426d92a37dc9466e55e8c0cc5fccf02d0107d1035f8ee1c43fb1539592174')
     version('0.5.0', sha256='2837b7371db3ac1bcc31a479d7cf0eb62a503cacadfa4187061502b3c4a89fa0')
 
     ###########################################################################
