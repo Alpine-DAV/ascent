3d33a74b078f61c87c53b831240aba0a754d3435
Matt Larsen <larsen30@llnl.gov>
2020-03-16 07:38:11 -0700

    relax tolerance

--- src/tests/ascent/t_ascent_rover.cpp
+++ src/tests/ascent/t_ascent_rover.cpp
@@ -289,7 +289,7 @@ TEST(ascent_rover, test_volume_min_max)
     ascent.close();
 
     // check that we created an image
-    EXPECT_TRUE(check_test_image(output_file));
+    EXPECT_TRUE(check_test_image(output_file, 0.01f, "100"));
     std::string msg = "An example of using the volume (unstructured grid) extract with "
                       "min and max values.";
     ASCENT_ACTIONS_DUMP(actions,output_file,msg);
@@ -359,7 +359,7 @@ TEST(ascent_rover, test_volume_serial)
     ascent.close();
 
     // check that we created an image
-    EXPECT_TRUE(check_test_image(output_file));
+    EXPECT_TRUE(check_test_image(output_file, 0.01f, "100"));
     std::string msg = "An example of using the volume (unstructured grid) extract.";
     ASCENT_ACTIONS_DUMP(actions,output_file,msg);
 }
