e12264cd341f0a92b3d5485782a1e0dbbcd1fa58
Cyrus Harrison <cyrush@llnl.gov>
2020-04-01 11:38:17 -0700

    update pascal script

--- scripts/spack_install/llnl_pascal_gcc_4.9.3_openmp_install.sh
+++ scripts/spack_install/llnl_pascal_gcc_4.9.3_openmp_install.sh
@@ -3,8 +3,9 @@
 #
 date
 # run spack install, this will install ascent@develop
+export BASE_DIR=/usr/gapps/conduit/software/ascent
 export ASCENT_VERSION=0.5.2-pre
-export DEST_DIR=/usr/gapps/conduit/ascent/${ASCENT_VERSION}/toss_3_x86_64_ib/openmp/gnu
+export DEST_DIR=${BASE_DIR}/${ASCENT_VERSION}/toss_3_x86_64_ib/openmp/gnu
 mkdir -P $DEST_DIR
 python scripts/uberenv/uberenv.py --spec="%gcc" \
        --install \
@@ -15,7 +16,7 @@ python scripts/uberenv/uberenv.py --spec="%gcc" \
 rm public_env.sh
 python scripts/spack_install/gen_public_install_env_script.py ${DEST_DIR} gcc/4.9.3
 chmod a+x public_env.sh
-cp public_env.sh /usr/gapps/conduit/ascent/${ASCENT_VERSION}/toss_3_x86_64_ib/ascent_toss_3_x86_64_ib_setup_env_gcc_openmp.sh
-chmod a+rX -R /usr/gapps/conduit/ascent/${ASCENT_VERSION}/
+cp public_env.sh ${BASE_DIR}/${ASCENT_VERSION}/toss_3_x86_64_ib/ascent_toss_3_x86_64_ib_setup_env_gcc_openmp.sh
+chmod a+rX -R /usr/gapps/conduit/software/ascent
 date
 
