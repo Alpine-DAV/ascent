fdb4ea579c7a7878cf25f69b8c22f8398d4626dd
Stefaryn <superstefan16@gmail.com>
2020-08-29 14:34:22 -0700

    no changes

--- src/ascent/runtimes/flow_filters/ascent_runtime_simplex_filters.cpp
+++ src/ascent/runtimes/flow_filters/ascent_runtime_simplex_filters.cpp
@@ -553,8 +553,8 @@ CameraSimplex::execute()
 
     // Testing specific scores
 /*
-    winning_i = 38;
-    winning_j = 5;
+    winning_i = 30;
+    winning_j = 12;
 
     Camera cam = GetCamera3(xMin, xMax, yMin, yMax, zMin, zMax,
         	        radius, winning_i, numTheta, winning_j, numPhi, focus); 
