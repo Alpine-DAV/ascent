c866652e522dce11db8ccda3601f6ebd84a339b9
Cyrus Harrison <cyrush@llnl.gov>
2020-10-30 21:10:20 -0700

    fix bug with graph build bookkeeping (#625)

--- src/tests/ascent/python/t_python_ascent_render.py
+++ src/tests/ascent/python/t_python_ascent_render.py
@@ -58,6 +58,8 @@ import conduit
 import conduit.blueprint
 import ascent
 
+import numpy as np
+
 class Test_Ascent_Render(unittest.TestCase):
     def test_render_2d(self):
         # if we don't have ascent, simply return
@@ -144,6 +146,61 @@ class Test_Ascent_Render(unittest.TestCase):
         a.execute(actions)
         a.close()
         self.assertTrue(os.path.isfile(ofile))
+
+    def test_delete_scene(self):
+        # exec again, but this time remove a scene
+        # tests regression related to internal book keeping
+        # with graph setup
+        
+        mesh = conduit.Node()
+        conduit.blueprint.mesh.examples.braid("hexs",
+                                              5,
+                                              5,
+                                              5,
+                                              mesh)
+        a = ascent.Ascent()
+        opts = conduit.Node()
+        opts["exceptions"] = "forward"
+        a.open(opts)
+        a.publish(mesh);
+
+        actions = conduit.Node()
+
+        add_act = actions.append()
+        add_act["action"] = "add_pipelines"
+        pipelines = add_act["pipelines"]
+        pipelines["pl1/f1/type"] = "contour"
+        contour_params = pipelines["pl1/f1/params"]
+        contour_params["field"] = "braid"
+        iso_vals = np.array([0.2, 0.4],dtype=np.float32)
+        contour_params["iso_values"].set(iso_vals)
+        
+        add_act2 = actions.append()
+        add_act2["action"] = "add_scenes"
+        scenes = add_act2["scenes"]
+        scenes["s1/plots/p1/type"] = "pseudocolor"
+        scenes["s1/plots/p1/pipeline"] = "pl1"
+        scenes["s1/plots/p1/field"] = "braid"
+        # set the output file name (ascent will add ".png")
+        scenes["s1/image_name"] = "out_pipeline_ex1_contour"
+        scenes["s2/plots/p1/type"] = "pseudocolor"
+        scenes["s2/plots/p1/pipeline"] = "pl1"
+        scenes["s2/plots/p1/field"] = "braid"
+        # set the output file name (ascent will add ".png")
+        scenes["s2/image_name"] = "out_pipeline_ex1_contour_blah"
+
+        # print our full actions tree
+        print(actions.to_yaml())
+        # execute the actions
+        a.execute(actions)
+
+        # now exec w/o s1
+        scenes.remove(path="s1")
+
+        # print our full actions tree
+        print(actions.to_yaml())
+        # execute the actions
+        a.execute(actions)
 
 
 if __name__ == '__main__':
