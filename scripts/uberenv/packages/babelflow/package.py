06e2661e7004c51ca812f5bd64154bb5beee02aa
Cyrus Harrison <cyrush@llnl.gov>
2021-02-18 11:19:31 -0800

    use tagged babelflow and pmt

--- scripts/uberenv/packages/babelflow/package.py
+++ scripts/uberenv/packages/babelflow/package.py
@@ -1,24 +1,7 @@
-# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
+# Copyright 2013-2021 Lawrence Livermore National Security, LLC and other
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
-
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install babelflow
-#
-# You can edit this file again by typing:
-#
-#     spack edit babelflow
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
 
 from spack import *
 
@@ -27,15 +10,11 @@ class Babelflow(CMakePackage):
     """FIXME: Put a proper description of your package here."""
 
     homepage = "https://github.com/sci-visus/BabelFlow"
-    url      = "https://github.com/sci-visus/BabelFlow/archive/ascent.zip"
+    url      = "https://github.com/sci-visus/BabelFlow/archive/v1.0.0.tar.gz"
 
     maintainers = ['spetruzza']
 
-    version('develop',
-            git='https://github.com/sci-visus/BabelFlow.git',
-            branch='ascent',
-            submodules=True,
-            preferred=True)
+    version('1.0.0',  sha256='4c4d7ddf60e25e8d3550c07875dba3e46e7c9e61b309cc47a409461b7ffa405e')
 
     depends_on('mpi')
 
