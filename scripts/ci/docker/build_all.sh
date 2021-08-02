87a5130bd0edfec6c94d71c55867721b3f5bae0c
Cyrus Harrison <cyrush@llnl.gov>
2021-08-02 15:44:18 -0700

    updates

--- scripts/ci/docker/build_all.sh
+++ scripts/ci/docker/build_all.sh
@@ -7,6 +7,7 @@
 cd alpinedav_ubuntu_18_devel
 ./build.sh 
 cd ..
+# + tpls
 cd alpinedav_ubuntu_18_devel_tpls
 ./build.sh
 cd ..
@@ -17,25 +18,26 @@ cd ..
 cd alpinedav_ubuntu_18_cuda_10.1_devel
 ./build.sh 
 cd ..
+# + tpls
 cd alpinedav_ubuntu_18_cuda_10.1_devel_tpls
 ./build.sh
-
+cd ..
 
 # ubuntu 20.10
 cd alpinedav_ubuntu_20.10_devel
 ./build.sh 
 cd ..
+# + tpls
 cd alpinedav_ubuntu_20.10_devel_tpls
 ./build.sh
 cd ..
-
 
 # ubuntu 21.04
 cd alpinedav_ubuntu_21.04_devel
 ./build.sh 
 cd ..
+# + tpls
 cd alpinedav_ubuntu_21.04_devel_tpls
 ./build.sh
 cd ..
-
 
