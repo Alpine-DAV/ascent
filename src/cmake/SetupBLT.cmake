5eab69b0fcd9d0383b331f1090e8ae833e500879
Cyrus Harrison <cyrush@llnl.gov>
2021-08-20 16:08:54 -0700

    adj for blt and mpi opts

---
+++ src/cmake/SetupBLT.cmake
@@ -0,0 +1,73 @@
+# Copyright (c) Lawrence Livermore National Security, LLC and other Ascent
+# Project developers. See top-level LICENSE AND COPYRIGHT files for dates and
+# other details. No copyright assignment is required to contribute to Ascent.
+
+################################################################
+# don't use BLT's all warnings feature
+################################################################
+set(ENABLE_ALL_WARNINGS OFF CACHE BOOL "")
+
+################################################################
+# if BLT_SOURCE_DIR is not set - use "blt" as default
+################################################################
+if(NOT BLT_SOURCE_DIR)
+    set(BLT_SOURCE_DIR "blt")
+endif()
+
+################################################################
+# if not set, prefer c++11 lang standard
+################################################################
+if(NOT BLT_CXX_STD)
+    set(BLT_CXX_STD "c++11" CACHE STRING "")
+endif()
+
+################################################################
+# if not set, prefer folder grouped targets
+################################################################
+if(NOT ENABLE_FOLDERS)
+    set(ENABLE_FOLDERS TRUE CACHE STRING "")
+endif()
+
+
+################################################################
+# init blt using BLT_SOURCE_DIR
+################################################################
+include(${BLT_SOURCE_DIR}/SetupBLT.cmake)
+
+if(ENABLE_MPI)
+    # on some platforms (mostly cray systems) folks skip mpi
+    # detection in BLT by setting ENABLE_FIND_MPI = OFF
+    # in these cases, we need to set FOUND_MPI = TRUE,
+    # since the rest of our cmake logic to include MPI uses FOUND_MPI
+    if(NOT ENABLE_FIND_MPI)
+        set(FOUND_MPI ON CACHE BOOL "")
+    endif()
+endif()
+
+################################################################
+# apply folders to a few ungrouped blt targets
+################################################################
+
+###############################################
+# group main blt docs targets into docs folder
+###############################################
+blt_set_target_folder( TARGET docs FOLDER docs)
+
+if(TARGET sphinx_docs)
+    blt_set_target_folder( TARGET sphinx_docs FOLDER docs)
+endif()
+
+if(TARGET doxygen_docs)
+    blt_set_target_folder( TARGET doxygen_docs FOLDER docs)
+endif()
+
+####################################################
+# group top level blt health checks into blt folder
+####################################################
+if(TARGET check)
+    blt_set_target_folder( TARGET check FOLDER blt)
+endif()
+
+if(TARGET style)
+    blt_set_target_folder( TARGET style FOLDER blt)
+endif()
