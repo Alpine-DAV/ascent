b5904a39d01059cca17b6fdf26e554f125a1f039
Matt Larsen <larsen30@llnl.gov>
2021-09-17 13:43:38 -0700

    bring more derived tests back online

--- src/tests/ascent/t_ascent_derived.cpp
+++ src/tests/ascent/t_ascent_derived.cpp
@@ -185,6 +185,109 @@ TEST(ascent_expressions, derived_simple)
 #endif
 
 
+
+//-----------------------------------------------------------------------------
+TEST(ascent_expressions, basic_derived_expressions)
+{
+  Node n;
+  ascent::about(n);
+  // only run this test if ascent was built with jit support
+  if(n["runtimes/ascent/jit/status"].as_string() == "disabled")
+  {
+      ASCENT_INFO("Ascent JIT support disabled, skipping test\n");
+      return;
+  }
+
+  //
+  // Create an example mesh.
+  //
+  Node data;
+  conduit::blueprint::mesh::examples::braid("uniform",
+                                            EXAMPLE_MESH_SIDE_DIM,
+                                            EXAMPLE_MESH_SIDE_DIM,
+                                            EXAMPLE_MESH_SIDE_DIM,
+                                            data);
+  // ascent normally adds this but we are doing an end around
+  data["state/domain_id"] = 0;
+  Node multi_dom;
+  blueprint::mesh::to_multi_domain(data, multi_dom);
+
+  runtime::expressions::register_builtin();
+  runtime::expressions::ExpressionEval eval(&multi_dom);
+
+  conduit::Node res;
+  std::string expr;
+
+  // expr = "1 + field('braid') + 1";
+  //    +
+  //  /   \
+  //  1   + return_type: jitable
+  //     / \
+  // field  1
+  //
+  // field + field + 1
+  //
+  //       +
+  //     /   \
+  // field   + return_type: jitable
+  //        / \
+  //    field  1
+  //
+  // max(field + 1)
+  //
+  //     max
+  //      |
+  //      + jitable -> field
+  //     / \
+  // field  1
+  //
+
+  expr = "f1 = max((2.0 + 1) / 0.5 + field('braid'), 0.0)\n"
+         "sum(f1 < 0)";
+  res = eval.evaluate(expr);
+  EXPECT_EQ(res["value"].to_float64(), 0);
+  EXPECT_EQ(res["type"].as_string(), "double");
+
+  expr = "method1 = field('braid') + 1 + field('braid') + 1\n"
+         "method2 = 2 * field('braid') + 2\n"
+         "bool_field = method1 != method2\n"
+         "sum(bool_field) == 0";
+
+  res = eval.evaluate(expr);
+  EXPECT_EQ(res["value"].to_uint8(), 1);
+  EXPECT_EQ(res["type"].as_string(), "bool");
+
+  // errors
+  // scalar * vector fields
+  bool threw = false;
+  try
+  {
+    expr = "field('braid') * field('vel')";
+    eval.evaluate(expr);
+  }
+  catch(...)
+  {
+    threw = true;
+  }
+  EXPECT_EQ(threw, true);
+
+  // incompatible number of entries
+  threw = false;
+  try
+  {
+    expr = "field('braid') * field('radial')";
+    eval.evaluate(expr);
+  }
+  catch(...)
+  {
+    threw = true;
+  }
+  EXPECT_EQ(threw, true);
+
+}
+
+//-----------------------------------------------------------------------------
+
 TEST(ascent_expressions, derived_mesh_specific_paths)
 {
   // Run on multiple meshes
@@ -313,223 +416,6 @@ TEST(ascent_expressions, derived_mesh_specific_paths)
 
   }
 }
-
-#if 0
-//-----------------------------------------------------------------------------
-TEST(ascent_expressions, derived_expressions)
-{
-  Node n;
-  ascent::about(n);
-  // only run this test if ascent was built with jit support
-  if(n["runtimes/ascent/jit/status"].as_string() == "disabled")
-  {
-      ASCENT_INFO("Ascent JIT support disabled, skipping test\n");
-      return;
-  }
-
-  //
-  // Create an example mesh.
-  //
-  Node data;
-  conduit::blueprint::mesh::examples::braid("uniform",
-                                            EXAMPLE_MESH_SIDE_DIM,
-                                            EXAMPLE_MESH_SIDE_DIM,
-                                            EXAMPLE_MESH_SIDE_DIM,
-                                            data);
-  // ascent normally adds this but we are doing an end around
-  data["state/domain_id"] = 0;
-  Node multi_dom;
-  blueprint::mesh::to_multi_domain(data, multi_dom);
-
-  runtime::expressions::register_builtin();
-  runtime::expressions::ExpressionEval eval(&multi_dom);
-
-  conduit::Node res;
-  std::string expr;
-
-  // expr = "1 + field('braid') + 1";
-  //    +
-  //  /   \
-  //  1   + return_type: jitable
-  //     / \
-  // field  1
-  //
-  // field + field + 1
-  //
-  //       +
-  //     /   \
-  // field   + return_type: jitable
-  //        / \
-  //    field  1
-  //
-  // max(field + 1)
-  //
-  //     max
-  //      |
-  //      + jitable -> field
-  //     / \
-  // field  1
-  //
-
-  expr = "f1 = max((2.0 + 1) / 0.5 + field('braid'), 0.0)\n"
-         "sum(f1 < 0)";
-  res = eval.evaluate(expr);
-  EXPECT_EQ(res["value"].to_float64(), 0);
-  EXPECT_EQ(res["type"].as_string(), "double");
-
-  expr = "method1 = field('braid') + 1 + field('braid') + 1\n"
-         "method2 = 2 * field('braid') + 2\n"
-         "bool_field = method1 != method2\n"
-         "sum(bool_field) == 0";
-
-  res = eval.evaluate(expr);
-  EXPECT_EQ(res["value"].to_uint8(), 1);
-  EXPECT_EQ(res["type"].as_string(), "bool");
-
-  // errors
-  // scalar * vector fields
-  bool threw = false;
-  try
-  {
-    expr = "field('braid') * field('vel')";
-    eval.evaluate(expr);
-  }
-  catch(...)
-  {
-    threw = true;
-  }
-  EXPECT_EQ(threw, true);
-
-  // incompatible number of entries
-  threw = false;
-  try
-  {
-    expr = "field('braid') * field('radial')";
-    eval.evaluate(expr);
-  }
-  catch(...)
-  {
-    threw = true;
-  }
-  EXPECT_EQ(threw, true);
-
-/*
-  // Run on multiple meshes
-  const std::vector<std::string> mesh_types = {
-      "uniform", "rectilinear", "structured", "tris", "quads", "hexs", "tets"};
-  const std::vector<std::vector<long long>> mesh_dims = {
-      {EXAMPLE_MESH_SIDE_DIM, EXAMPLE_MESH_SIDE_DIM, EXAMPLE_MESH_SIDE_DIM},
-      {EXAMPLE_MESH_SIDE_DIM, EXAMPLE_MESH_SIDE_DIM, 0}};
-
-  for(const std::string &mesh_type : mesh_types)
-  {
-    for(const auto &dims : mesh_dims)
-    {
-      if(((mesh_type == "hexs" || mesh_type == "tets") && dims[2] == 0) ||
-         (mesh_type == "quads" && dims[2] != 0))
-      {
-        continue;
-      }
-      Node data;
-      conduit::blueprint::mesh::examples::braid(
-          mesh_type, dims[0], dims[1], dims[2], data);
-
-      // ascent normally adds this but we are doing an end around
-      data["state/domain_id"] = 0;
-      Node multi_dom;
-      blueprint::mesh::to_multi_domain(data, multi_dom);
-
-      runtime::expressions::register_builtin();
-      runtime::expressions::ExpressionEval eval(&multi_dom);
-
-      conduit::Node res;
-      std::string expr;
-
-      expr = "min_val = min(field('braid')).value\n"
-             "max_val = max(field('braid')).value\n"
-             "norm_field = (field('braid') - min_val) / (max_val - min_val)\n"
-             "not_between_0_1 = not (norm_field >= 0 and norm_field <= 1)\n"
-             "sum(not_between_0_1)";
-      res = eval.evaluate(expr);
-      EXPECT_EQ(res["value"].to_float64(), 0);
-      EXPECT_EQ(res["type"].as_string(), "double");
-
-      expr = "avg(topo('mesh').cell.x)";
-      res = eval.evaluate(expr);
-      const double tiny = 1e-10;
-      EXPECT_EQ(std::abs(res["value"].to_float64()) < tiny, true);
-      EXPECT_EQ(res["type"].as_string(), "double");
-
-      expr = "builtin_avg = avg(sin(field('radial')))\n"
-             "num_elements = sum(derived_field(1.0, 'mesh', 'element'))\n"
-             "manual_avg = sum(sin(field('radial'))) / num_elements\n"
-             "builtin_avg == manual_avg";
-      res = eval.evaluate(expr);
-
-      EXPECT_EQ(res["type"].as_string(), "bool");
-
-      Node last;
-      runtime::expressions::ExpressionEval::get_last(last);
-      double manual = last["manual_avg/100/value"].to_float64();
-      double builtin = last["builtin_avg/100/value"].to_float64();
-      EXPECT_NEAR(manual, builtin, 1e-8);
-
-      // apparently vel is element assoc
-      if(dims[2] != 0 && (mesh_type == "uniform" || mesh_type == "rectilinear"))
-      {
-        expr = "builtin_vort = curl(field('vel'))\n"
-               "du = gradient(field('vel', 'u'))\n"
-               "dv = gradient(field('vel', 'v'))\n"
-               "dw = gradient(field('vel', 'w'))\n"
-               "w_x = dw.y - dv.z\n"
-               "w_y = du.z - dw.x\n"
-               "w_z = dv.x - du.y\n"
-               "not_eq = not (builtin_vort.x == w_x and\n"
-               "              builtin_vort.y == w_y and\n"
-               "              builtin_vort.z == w_z)\n"
-               "sum(not_eq)";
-        res = eval.evaluate(expr);
-        EXPECT_EQ(res["value"].to_float64(), 0);
-        EXPECT_EQ(res["type"].as_string(), "double");
-      }
-
-      if(dims[2] == 0)
-      {
-        expr = "sum(topo('mesh').cell.area)";
-        std::cout<<"Mesh type "<<mesh_type<<"\n";
-        std::cout<<"expression "<<expr<<"\n";
-        res = eval.evaluate(expr);
-        res.print();
-        EXPECT_NEAR(res["value"].to_float64(), 400.0, 1e-8);
-
-        expr = "topo('mesh').cell.volume";
-        EXPECT_ANY_THROW(eval.evaluate(expr));
-      }
-      else
-      {
-        // expr = "topo('mesh').cell.area";
-        // EXPECT_ANY_THROW(eval.evaluate(expr));
-
-        // expr = "topo('mesh').cell.volume";
-        // eval.evaluate(expr);
-      }
-
-      if(mesh_type == "uniform" || mesh_type == "rectilinear" ||
-         mesh_type == "structured")
-      {
-        // element to vertex
-        expr = "recenter(field('radial') + 1)";
-        eval.evaluate(expr);
-      }
-
-      // vertex to element
-      expr = "recenter(field('braid'))";
-      eval.evaluate(expr);
-    }
-  }
-*/
-}
-
 //-----------------------------------------------------------------------------
 
 TEST(ascent_expressions, braid_sample)
@@ -540,6 +426,12 @@ TEST(ascent_expressions, braid_sample)
   if(n["runtimes/ascent/jit/status"].as_string() == "disabled")
   {
       ASCENT_INFO("Ascent JIT support disabled, skipping test\n");
+      return;
+  }
+    // only run this test if ascent was built with vtkm support
+  if(n["runtimes/ascent/vtkm/status"].as_string() == "disabled")
+  {
+      ASCENT_INFO("Ascent vtkm support disabled, skipping test");
       return;
   }
 
@@ -600,7 +492,7 @@ TEST(ascent_expressions, braid_sample)
 
   EXPECT_TRUE(check_test_image(output_image, 0.1));
 }
-#endif
+
 #if 0
 TEST(ascent_expressions, multi_topos)
 {
