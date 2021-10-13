981166b814d7f1a851a7e513d10a7151af66a239
Justin Privitera <35237779+JustinPrivitera@users.noreply.github.com>
2021-10-13 09:51:06 -0700

    Adding to Transmogrifier to support Polyhedral Meshes (#778)
    * added function definitions
    
    * implementation for to_poly
    
    * removed dead code
    
    * everything should be hooked up properly
    
    * fixed failing tests and made changes based on feedback
    
    * cleaned up transmogrifier code
    
    * removed dead code
    
    * made changes based on requested feedback
    
    * added tests for this functionality
    
    * added multidomain tests
    
    * added test for multiple meshes stored in one tree
    
    * make poly tests only run when vtk-h is on
    
    * missed a few opt cases
    
    * one more
    
    Co-authored-by: Cyrus Harrison <cyrush@llnl.gov>

--- src/ascent/runtimes/ascent_data_object.cpp
+++ src/ascent/runtimes/ascent_data_object.cpp
@@ -324,11 +324,29 @@ std::shared_ptr<VTKHCollection> DataObject::as_vtkh_collection()
     }
 
     bool zero_copy = true;
+    conduit::Node n_poly;
+    conduit::Node *to_vtkh = nullptr;
+    
+    if (m_low_bp != nullptr)
+    {
+      if (Transmogrifier::is_poly(*m_low_bp))
+      {
+        Transmogrifier::to_poly(*m_low_bp, n_poly);
+        to_vtkh = &n_poly;
+        zero_copy = false;
+      }
+      else
+      {
+        to_vtkh = &(*m_low_bp);
+      }
+    }
+
     // convert to vtkh
     std::shared_ptr<VTKHCollection>
-      vtkh_dset(VTKHDataAdapter::BlueprintToVTKHCollection(*m_low_bp, zero_copy));
+      vtkh_dset(VTKHDataAdapter::BlueprintToVTKHCollection(*to_vtkh, zero_copy));
 
-     m_vtkh = vtkh_dset;
+    m_vtkh = vtkh_dset;
+    
     return m_vtkh;
   }
 
--- src/ascent/runtimes/ascent_transmogrifier.cpp
+++ src/ascent/runtimes/ascent_transmogrifier.cpp
@@ -55,6 +55,8 @@
 #include "ascent_mfem_data_adapter.hpp"
 #endif
 #include "ascent_logging.hpp"
+#include <conduit_blueprint.hpp>
+#include <algorithm>
 
 //-----------------------------------------------------------------------------
 // -- begin ascent:: --
@@ -105,6 +107,82 @@ conduit::Node* Transmogrifier::low_order(conduit::Node &dataset)
   ASCENT_ERROR("Unable to convert high order mesh when MFEM is not enabled");
   return nullptr;
 #endif
+}
+
+bool Transmogrifier::is_poly(const conduit::Node &doms)
+{
+  const int num_domains = doms.number_of_children();
+
+  for (int i = 0; i < num_domains; i ++)
+  {
+    const conduit::Node &dom = doms.child(i);
+    conduit::NodeConstIterator itr = dom["topologies"].children();
+    while (itr.has_next())
+    {
+      const conduit::Node &topo = itr.next();
+      if (topo.has_child("elements"))
+      {
+        if (topo["elements"].has_child("shape"))
+        {
+          if (topo["elements/shape"].as_string() == "polyhedral" || 
+              topo["elements/shape"].as_string() == "polygonal")
+          {
+            return true;
+          }
+        }
+      }
+    }
+  }
+
+  return false;
+}
+
+// to_poly assumes that the node n is polyhedral
+void Transmogrifier::to_poly(conduit::Node &doms, conduit::Node &to_vtkh)
+{
+  const int num_domains = doms.number_of_children();
+
+  for (int i = 0; i < num_domains; i ++)
+  {
+    const conduit::Node &dom = doms.child(i);
+    std::vector<std::string> poly_topos;
+    conduit::Node &res = to_vtkh.append();
+
+    // we know it must have a child "topologies" b/c otherwise it is not valid blueprint
+    conduit::NodeConstIterator itr = dom["topologies"].children();
+    while (itr.has_next())
+    {
+      const conduit::Node &topo = itr.next();
+      if (topo.has_child("elements"))
+      {
+        if (topo["elements"].has_child("shape"))
+        {
+          if (topo["elements/shape"].as_string() == "polyhedral" || 
+              topo["elements/shape"].as_string() == "polygonal")
+          {
+            poly_topos.push_back(topo.name());
+          }
+        }
+      }
+    }
+
+    res.set_external(dom);
+
+    std::vector<std::string> coordsets;
+    for (int i = 0; i < poly_topos.size(); i ++)
+    {
+      conduit::Node s2dmap, d2smap, options;
+      coordsets.push_back(dom["topologies/" + poly_topos[i] + "/coordset"].as_string());
+      conduit::blueprint::mesh::topology::unstructured::generate_sides(
+        dom["topologies/" + poly_topos[i]],
+        res["topologies/" + poly_topos[i]],
+        res["coordsets/" + coordsets[coordsets.size() - 1]],
+        res["fields"],
+        s2dmap,
+        d2smap,
+        options);
+    }
+  }
 }
 
 //-----------------------------------------------------------------------------
