f5adca3b78b7c1dcace8622485e2d1c4ffb05363
Cyrus Harrison <cyrush@llnl.gov>
2021-11-03 09:52:13 -0700

    update docker examples

--- src/examples/docker/ubuntu/build.sh
+++ src/examples/docker/ubuntu/build.sh
@@ -3,11 +3,11 @@
 # Project developers. See top-level LICENSE AND COPYRIGHT files for dates and
 # other details. No copyright assignment is required to contribute to Ascent.
 
-export TAG_BASE=alpinedav/ascent:ascent-jupyter-ubuntu-18
+export TAG_BASE=alpinedav/ascent:ascent-ubuntu-18
 
 date
 
-python ../build_and_tag.py ${TAG_BASE}
+python build_and_tag.py ${TAG_BASE}
 
 date
 
