7ed2999f96bf08bf17bc4bed896ce527307eefe2
Cyrus Harrison <cyrush@llnl.gov>
2022-02-10 09:21:18 -0800

    genten install script updates (perlmutter + summit) (#879)
    * update for perlmutter genten install script
    * update for summit genten install script

--- scripts/spack_install/olcf_summit_gcc_9.3.0_cuda_genten_install.sh
+++ scripts/spack_install/olcf_summit_gcc_9.3.0_cuda_genten_install.sh
@@ -6,24 +6,22 @@ set -e
 date
 # run spack install, this will install ascent@develop
 export ASCENT_VERSION=0.8.0_genten
-export BASE_DIR=$PSCRATCH/ASCENT_INSTALL 
-#/project/projectdirs/alpine/software/ascent/
-export DEST_DIR=$BASE_DIR/${ASCENT_VERSION}/
+export BASE_DIR=$WORLDWORK/csc340/software/ascent
+export DEST_DIR=$BASE_DIR/${ASCENT_VERSION}/summit/cuda/gnu
 mkdir -p $DEST_DIR
-# ^vtk-h~blt_find_mpi
-python3 scripts/uberenv/uberenv.py --spec="%gcc~blt_find_mpi+genten ^conduit~blt_find_mpi ^vtk-h~blt_find_mpi ^kokkos+wrapper  ^hdf5~mpi ^cmake~openssl~ncurses" \
+python3 scripts/uberenv/uberenv.py --spec="%gcc~test+genten ^kokkos+wrapper" \
        --pull \
        --install \
-       --spack-config-dir="scripts/uberenv_configs/spack_configs/configs/nersc/perlmutter_gcc_10.3.0_cuda_11.4/" \
+       --spack-config-dir="scripts/uberenv_configs/spack_configs/configs/olcf/summit_gcc_9.3.0_cuda_11.0.3/" \
        --prefix=${DEST_DIR}
 
 # gen symlinks to important deps
 python3 scripts/spack_install/gen_extra_install_symlinks.py ${DEST_DIR} cmake conduit
 # gen env helper script
 rm -f public_env.sh
-python3 scripts/spack_install/gen_public_install_env_script.py ${DEST_DIR} PrgEnv-gnu cpe-cuda/21.12  cudatoolkit/21.9_11.4 
+python3 scripts/spack_install/gen_public_install_env_script.py ${DEST_DIR} gcc/9.3.0 cuda
 chmod a+x public_env.sh
-cp public_env.sh $BASE_DIR/${ASCENT_VERSION}/ascent_permutter_setup_env_gcc_cuda.sh
+cp public_env.sh $BASE_DIR/${ASCENT_VERSION}/summit/ascent_summit_setup_env_gcc_cuda.sh
 # change perms to group write
 chgrp -R csc340 $BASE_DIR/${ASCENT_VERSION}
 chmod g+rwX -R $BASE_DIR/${ASCENT_VERSION}
