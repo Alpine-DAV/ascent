7b13e58135c26cbde22458de9e5595a1c51feec8
Cyrus Harrison <cyrush@llnl.gov>
2022-03-15 14:38:45 -0700

    ums and env path update for summit 0.8.0 openmp install

--- src/docs/sphinx/tutorial_setup_olcf_summit_openmp_test.sh
+++ src/docs/sphinx/tutorial_setup_olcf_summit_openmp_test.sh
@@ -2,7 +2,8 @@
 #
 # source helper script that loads modules, sets python paths, and ASCENT_DIR env var
 #
-source /gpfs/alpine/csc340/world-shared/software/ascent/current/summit/ascent_summit_setup_env_gcc_openmp.sh
+source /sw/summit/ums/ums010/ascent/current/summit/ascent_summit_setup_env_gcc_openmp.sh
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/sw/summit/ums/ums010/ascent/current/summit/openmp/gnu/python-install/lib/
 
 #
 # make your own dir to hold the tutorial examples
@@ -13,7 +14,7 @@ cd ascent_tutorial
 #
 # copy the examples from the public install
 #
-cp -r /gpfs/alpine/world-shared/csc340/software/ascent/current/summit/openmp/gnu/ascent-install/examples/ascent/tutorial/* .
+cp -r /sw/summit/ums/ums010/ascent/current/summit/openmp/gnu/ascent-install/examples/ascent/tutorial/* .
 
 #
 # build cpp examples and run the first one
