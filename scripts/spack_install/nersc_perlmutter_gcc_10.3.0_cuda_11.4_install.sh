ee25e81d599cc8945b869c07185fee1008b2e59c
Cyrus Harrison <cyrush@llnl.gov>
2022-03-22 13:51:04 -0700

    perlmutter genten public install updates (#898)
    * update install loc or genten script
    
    * fix spec typo for cuda perlmutter
    
    * update perlmutter configs (module changes)
    
    * updated install scripts for perlmutter
    
    * fix submod problem

--- scripts/spack_install/nersc_perlmutter_gcc_10.3.0_cuda_11.4_install.sh
+++ scripts/spack_install/nersc_perlmutter_gcc_10.3.0_cuda_11.4_install.sh
@@ -8,11 +8,11 @@ date
 export ASCENT_VERSION=0.8.0
 #/project/projectdirs/alpine/software/ascent/
 # CFS is the new name for /project/
-export BASE_DIR=/global/cfs/cdirs/alpine/software/ascent/ 
+export BASE_DIR=/global/cfs/cdirs/alpine/software/ascent/
 export DEST_DIR=$BASE_DIR/${ASCENT_VERSION}/perlmutter/cuda/gnu
 mkdir -p $DEST_DIR
 #
-python3 scripts/uberenv/uberenv.py --spec="%gcc~blt_find_mpi ^conduit~blt_find_mpi ^vtk-h@~blt_find_mpi ^hdf5~mpi ^cmake~openssl~ncurses" \
+python3 scripts/uberenv/uberenv.py --spec="%gcc~blt_find_mpi ^conduit~blt_find_mpi ^vtk-h~blt_find_mpi ^hdf5~mpi ^cmake~openssl~ncurses" \
        --pull \
        --install \
        --spack-config-dir="scripts/uberenv_configs/spack_configs/configs/nersc/perlmutter_gcc_10.3.0_cuda_11.4/" \
@@ -22,7 +22,7 @@ python3 scripts/uberenv/uberenv.py --spec="%gcc~blt_find_mpi ^conduit~blt_find_m
 python3 scripts/spack_install/gen_extra_install_symlinks.py ${DEST_DIR} cmake conduit
 # gen env helper script
 rm -f public_env.sh
-python3 scripts/spack_install/gen_public_install_env_script.py ${DEST_DIR} PrgEnv-gnu cudatoolkit/21.9_11.4  cpe-cuda/21.12
+python3 scripts/spack_install/gen_public_install_env_script.py ${DEST_DIR} PrgEnv-gnu cudatoolkit/11.4 cpe-cuda/22.02
 chmod a+x public_env.sh
 cp public_env.sh $BASE_DIR/${ASCENT_VERSION}/perlmutter/ascent_permutter_setup_env_gcc_cuda.sh
 # change perms to world readable
