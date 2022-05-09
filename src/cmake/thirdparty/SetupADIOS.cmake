5555b51d1f167ba25b1ea0b5ce0ed877b567f0b3
Ryan Krattiger <ryan.krattiger@kitware.com>
2022-05-09 11:53:20 -0500

    Update search paths for ADIOS(2).
    Fixes #916

--- src/cmake/thirdparty/SetupADIOS.cmake
+++ src/cmake/thirdparty/SetupADIOS.cmake
@@ -10,32 +10,32 @@
 #
 ###############################################################################
 
-# first Check for ADIOS_DIR
+# first Check for ADIOS_ROOT
 
-if(NOT ADIOS_DIR)
-    MESSAGE(FATAL_ERROR "ADIOS support needs explicit ADIOS_DIR")
+if(NOT ADIOS_ROOT)
+    MESSAGE(FATAL_ERROR "ADIOS support needs explicit ADIOS_ROOT")
 endif()
 
-MESSAGE(STATUS "Looking for ADIOS using ADIOS_DIR = ${ADIOS_DIR}")
+MESSAGE(STATUS "Looking for ADIOS using ADIOS_ROOT = ${ADIOS_ROOT}")
 
 # CMake's FindADIOS module uses the ADIOS_ROOT env var
-set(ADIOS_ROOT ${ADIOS_DIR})
+set(ADIOS_ROOT ${ADIOS_ROOT})
 set(ENV{ADIOS_ROOT} ${ADIOS_ROOT})
 
 # Use CMake's FindADIOS module, which uses hdf5's compiler wrappers to extract
 # all the info about the hdf5 install
-include(${ADIOS_DIR}/etc/FindADIOS.cmake)
+include(${ADIOS_ROOT}/etc/FindADIOS.cmake)
 
-# FindADIOS sets ADIOS_DIR to it's installed CMake info if it exists
-# we want to keep ADIOS_DIR as the root dir of the install to be
+# FindADIOS sets ADIOS_ROOT to it's installed CMake info if it exists
+# we want to keep ADIOS_ROOT as the root dir of the install to be
 # consistent with other packages
 
-set(ADIOS_DIR ${ADIOS_ROOT} CACHE PATH "" FORCE)
+set(ADIOS_ROOT ${ADIOS_ROOT} CACHE PATH "" FORCE)
 # not sure why we need to set this, but we do
 #set(ADIOS_FOUND TRUE CACHE PATH "" FORCE)
 
 if(NOT ADIOS_FOUND)
-    message(FATAL_ERROR "ADIOS_DIR is not a path to a valid ADIOS install")
+    message(FATAL_ERROR "ADIOS_ROOT is not a path to a valid ADIOS install")
 endif()
 
 blt_register_library(NAME adios
