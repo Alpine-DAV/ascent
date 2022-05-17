03cf56e5eb42e5b8a72a7806c9df07736024ebbb
Cyrus Harrison <cyrush@llnl.gov>
2022-05-17 15:52:59 -0700

    add missing files and dray_config.mk prototype

---
+++ src/cmake/thirdparty/SetupRAJA.cmake
@@ -0,0 +1,15 @@
+# Copyright (c) Lawrence Livermore National Security, LLC and other Ascent
+# Project developers. See top-level LICENSE AND COPYRIGHT files for dates and
+# other details. No copyright assignment is required to contribute to Ascent.
+
+message(STATUS "Looking for RAJA in: ${RAJA_DIR}")
+if (NOT RAJA_DIR)
+  message(FATAL_ERROR "Must specify 'RAJA_DIR'")
+endif()
+
+set(RAJA_BASE_DIR ${RAJA_DIR})
+find_dependency(RAJA REQUIRED
+                NO_DEFAULT_PATH
+                PATHS ${RAJA_DIR}/share/raja/cmake)
+
+message(STATUS "Found RAJA")
