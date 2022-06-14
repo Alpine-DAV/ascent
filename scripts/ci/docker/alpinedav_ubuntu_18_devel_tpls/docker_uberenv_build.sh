49ee14cd4963d552fb0250e3426dc7fdb5560dce
Cyrus Harrison <cyrush@llnl.gov>
2022-06-13 18:20:20 -0700

    fixes

--- scripts/ci/docker/alpinedav_ubuntu_18_devel_tpls/docker_uberenv_build.sh
+++ scripts/ci/docker/alpinedav_ubuntu_18_devel_tpls/docker_uberenv_build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#set -ev
+set -ev
 
 # variants
 # TODO:
 # (+python) python currently disabled, spack checksum fetch issues with multiple versions (3.9 and 3.8)
 # (+genten) genten currently disabled, wait for genten master to gain cokurt
 # (^vtk-m@1.8.0-rc1) use vtk-m 1.8 rc branch  (currently 1.7.1)
-export SPACK_SPEC="%gcc+mpi~python+dray+mfem+occa+fides+adios2+babelflow~genten ^vtk-m@1.7.1"
+export SPACK_SPEC="%gcc+mpi~python+dray+mfem+occa+fides+adios2+babelflow~genten"
 # constraints
-export SPACK_SPEC="${SPACK_SPEC}"
+export SPACK_SPEC="${SPACK_SPEC} ^vtk-m@1.7.1"
 # config
 export SPACK_CONFIG="scripts/uberenv_configs/spack_configs/configs/alpinedav/ubuntu_18_devel/"
 
