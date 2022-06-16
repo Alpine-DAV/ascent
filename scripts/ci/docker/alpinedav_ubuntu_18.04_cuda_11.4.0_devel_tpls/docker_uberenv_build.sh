66a01d3411d065b9af853d3240edf383232dab81
Cyrus Harrison <cyrush@llnl.gov>
2022-06-16 16:34:28 -0700

    updates, more updates, and more updates

--- scripts/ci/docker/alpinedav_ubuntu_18.04_cuda_11.4.0_devel_tpls/docker_uberenv_build.sh
+++ scripts/ci/docker/alpinedav_ubuntu_18.04_cuda_11.4.0_devel_tpls/docker_uberenv_build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-set -ev
+#set -ev
 
 # variants
 # TODO:
 # (+genten) genten currently disabled, wait for genten master to gain cokurt
 # (^vtk-m@1.8.0-rc1) use vtk-m 1.8 rc branch  (currently 1.7.1)
-export SPACK_SPEC="%gcc+mpi+cuda+vtkh+dray+mfem+occ~genten~python~openmp~shared"
+export SPACK_SPEC="%gcc+mpi+cuda+vtkh+dray+mfem+occa~genten~python~openmp~shared"
 # constraints
-export SPACK_SPEC="${SPACK_SPEC} ^hdf5~mpi ^dray~openmp+cuda ^kokkos+wrapper ^mfem~cuda cuda_arch=none ^hypre~cuda cuda_arch=none  ^vtk-h+cuda ^vtk-m+cuda@1.7.1 ^cmake~openssl~ncurses"
+export SPACK_SPEC="${SPACK_SPEC} ^hdf5~mpi ^dray~openmp+cuda ^kokkos+wrapper ^mfem~cuda ^hypre~cuda ^vtk-h+cuda ^vtk-m+cuda@1.7.1"
 # config
 export SPACK_CONFIG="scripts/uberenv_configs/spack_configs/configs/alpinedav/ubuntu_18.04_cuda_11.4.0_devel/"
 
@@ -22,3 +22,6 @@ cd ascent && python scripts/uberenv/uberenv.py \
 
 # change perms
 chmod -R a+rX /uberenv_libs
+
+# back to where we started
+cd ../
