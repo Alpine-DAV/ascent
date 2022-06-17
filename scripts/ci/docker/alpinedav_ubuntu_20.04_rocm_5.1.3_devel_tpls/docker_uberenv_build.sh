6b72358d45e2032abe1cb012d24a7999b1209c70
Cyrus Harrison <cyrush@llnl.gov>
2022-06-16 17:22:15 -0700

    power on more matrix

--- scripts/ci/docker/alpinedav_ubuntu_20.04_rocm_5.1.3_devel_tpls/docker_uberenv_build.sh
+++ scripts/ci/docker/alpinedav_ubuntu_20.04_rocm_5.1.3_devel_tpls/docker_uberenv_build.sh
@@ -6,9 +6,10 @@
 # (+genten) genten currently disabled, wait for genten master to gain cokurt
 # (+dray) rocm support isn't in dray pkg, we need to move to intl ver and +raja opt
 # (^vtk-m@1.8.0-rc1) use vtk-m 1.8 rc branch  (currently 1.7.1)
+# (^vtk-h+rocm) rocm support isn't in vtk-h pkg yet
 export SPACK_SPEC="%gcc+mpi+cuda+vtkh~dray+mfem+occa~genten~python~openmp~shared"
 # constraints
-export SPACK_SPEC="${SPACK_SPEC} ^hdf5~mpi ^kokkos+wrapper ^mfem~rocm ^hypre~rocm ^vtk-h+rocm ^vtk-m+rocm@1.7.1"
+export SPACK_SPEC="${SPACK_SPEC} ^hdf5~mpi ^kokkos+wrapper ^mfem~rocm ^hypre~rocm ^vtk-h ^vtk-m+rocm@1.7.1"
 # config
 export SPACK_CONFIG="scripts/uberenv_configs/spack_configs/configs/alpinedav/ubuntu_20.04_rocm_5.1.3_devel/"
 
