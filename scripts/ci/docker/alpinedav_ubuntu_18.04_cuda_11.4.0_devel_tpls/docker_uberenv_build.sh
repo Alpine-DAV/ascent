da323f21e1f17c3a69f1a9c87731c2ac38e3a4bf
Cyrus Harrison <cyrush@llnl.gov>
2022-06-18 09:09:16 -0700

    fixins

--- scripts/ci/docker/alpinedav_ubuntu_18.04_cuda_11.4.0_devel_tpls/docker_uberenv_build.sh
+++ scripts/ci/docker/alpinedav_ubuntu_18.04_cuda_11.4.0_devel_tpls/docker_uberenv_build.sh
@@ -5,12 +5,12 @@
 # TODO:
 # (+genten) genten currently disabled, wait for genten master to gain cokurt
 # (^vtk-m@1.8.0-rc1) use vtk-m 1.8 rc branch  (currently 1.7.1)
-export SPACK_SPEC="%gcc+mpi+cuda+vtkh+dray+mfem+occa~genten~python~openmp~shared"
+# (+dray) issue with umpire linking (solve in amlaga tests?)
+export SPACK_SPEC="%gcc+mpi+cuda+vtkh~dray+mfem+occa~genten~python~openmp~shared"
 # constraints
-export SPACK_SPEC="${SPACK_SPEC} ^hdf5~mpi ^dray~openmp+cuda ^kokkos+wrapper ^mfem~cuda~petsc ^hypre~cuda ^vtk-h+cuda ^vtk-m+cuda~kokkos@1.7.1"
+export SPACK_SPEC="${SPACK_SPEC} ^hdf5~mpi ^dray~openmp+cuda ^mfem~cuda~petsc~sundials~slep ^hypre~cuda ^vtk-h+cuda ^vtk-m+cuda~kokkos@1.7.1"
 # config
-export SPACK_CONFIG="scripts/uberenv_configs/spack_configs/configs/alpinedav/ubuntu_18.04_cuda_11.4.0_devel/"
-
+export SPACK_SPEC="${SPACK_SPEC} ^hdf5~mpi ^dray~openmp+cuda c ^hypre~cuda ^vtk-h+cuda ^vtk-m+cuda~kokkos@1.7.1"
 cd ascent && python scripts/uberenv/uberenv.py \
      -k \
      --spec="${SPACK_SPEC}" \
