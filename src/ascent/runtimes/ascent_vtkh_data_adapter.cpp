3ae4c4889bb28db1b0060a9d83d7ea0ebf41ffd8
Nicole Marsaglia <marsagli@uoreogn.edu>
2022-08-29 12:24:26 -0700

    nonzero works

--- src/ascent/runtimes/ascent_vtkh_data_adapter.cpp
+++ src/ascent/runtimes/ascent_vtkh_data_adapter.cpp
@@ -557,9 +557,9 @@ VTKHDataAdapter::BlueprintToVTKHCollection(const conduit::Node &n,
     const std::vector<std::string> topo_names  = dom["topologies"].child_names();
     const std::string topo_name = topo_names[0];
 
-    if(allCycles.size() > 1 && detail::allEqual(allCycles))
+    if(allCycles.size() != 0 && detail::allEqual(allCycles))
       datasets[topo_name].SetCycle(allCycles[0]);
-    if(allTimes.size() > 1 && detail::allEqual(allTimes))
+    if(allTimes.size() != 0 && detail::allEqual(allTimes))
       datasets[topo_name].SetTime(allTimes[0]);
 
 
