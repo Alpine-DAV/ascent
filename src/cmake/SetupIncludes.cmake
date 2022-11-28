c5f1f5dd356cc2337da29df33804a4fd23d8e186
Cyrus Harrison <cyrush@llnl.gov>
2022-11-28 10:27:45 -0800

    move ascent under libs

--- src/cmake/SetupIncludes.cmake
+++ src/cmake/SetupIncludes.cmake
@@ -15,17 +15,4 @@ include_directories(${PROJECT_SOURCE_DIR}/thirdparty_builtin/lodepng)
 # add tiny_obj_loader include dir
 include_directories(${PROJECT_SOURCE_DIR}/thirdparty_builtin/tiny_obj)
 
-#############
-# TODO: Fix these with build vs install interface target includes
-#############
-
-# add include dirs so units tests have access to the headers across
-# libs and in unit tests
-include_directories(${PROJECT_SOURCE_DIR}/ascent/)
-include_directories(${PROJECT_SOURCE_DIR}/ascent/c)
-include_directories(${PROJECT_BINARY_DIR}/ascent/)
-include_directories(${PROJECT_SOURCE_DIR}/ascent/utils)
-include_directories(${PROJECT_SOURCE_DIR}/ascent/runtimes)
-include_directories(${PROJECT_SOURCE_DIR}/ascent/hola)
-include_directories(${PROJECT_SOURCE_DIR}/ascent/runtimes/flow_filters)
 
