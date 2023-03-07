4484076c7b28ab15c8ae7f15a1322ecec43be386
Nicole Marsaglia <marsagli@uoreogn.edu>
2023-03-07 13:22:01 -0800

    clean up vtkmThreshold -- no longer needs worklet in stripPermutation

--- src/libs/vtkh/vtkm_filters/vtkmThreshold.cpp
+++ src/libs/vtkh/vtkm_filters/vtkmThreshold.cpp
@@ -2,7 +2,6 @@
 
 #include <vtkm/filter/entity_extraction/Threshold.h>
 #include <vtkm/cont/CellSetPermutation.h>
-#include <vtkm/worklet/CellDeepCopy.h>
 
 
 namespace vtkh
@@ -19,50 +18,6 @@ typedef vtkm::cont::CellSetPermutation<vtkm::cont::CellSetExplicit<>>
 typedef  vtkm::cont::CellSetPermutation<vtkm::cont::CellSetSingleType<>>
   PermExplicitSingle;
 
-void StripPermutation(vtkm::cont::DataSet &data_set)
-{
-  vtkm::cont::UnknownCellSet cell_set = data_set.GetCellSet();
-  vtkm::cont::DataSet result;
-  vtkm::cont::CellSetExplicit<> explicit_cells;
-
-  if(cell_set.IsType<PermStructured2d>())
-  {
-    PermStructured2d perm = cell_set.AsCellSet<PermStructured2d>();
-    explicit_cells = vtkm::worklet::CellDeepCopy::Run(perm);
-  }
-  else if(cell_set.IsType<PermStructured3d>())
-  {
-    PermStructured3d perm = cell_set.AsCellSet<PermStructured3d>();
-    explicit_cells = vtkm::worklet::CellDeepCopy::Run(perm);
-  }
-  else if(cell_set.IsType<PermExplicit>())
-  {
-    PermExplicit perm = cell_set.AsCellSet<PermExplicit>();
-    explicit_cells = vtkm::worklet::CellDeepCopy::Run(perm);
-  }
-  else if(cell_set.IsType<PermExplicitSingle>())
-  {
-    PermExplicitSingle perm = cell_set.AsCellSet<PermExplicitSingle>();
-    explicit_cells = vtkm::worklet::CellDeepCopy::Run(perm);
-  }
-
-  result.SetCellSet(explicit_cells);
-
-  vtkm::Id num_coords = data_set.GetNumberOfCoordinateSystems();
-  for(vtkm::Id i = 0; i < num_coords; ++i)
-  {
-    result.AddCoordinateSystem(data_set.GetCoordinateSystem(i));
-  }
-
-  vtkm::Id num_fields = data_set.GetNumberOfFields();
-  for(vtkm::Id i = 0; i < num_fields; ++i)
-  {
-    result.AddField(data_set.GetField(i));
-  }
-
-  data_set = result;
-}
-
 vtkm::cont::DataSet
 vtkmThreshold::Run(vtkm::cont::DataSet &input,
                    std::string field_name,
@@ -78,7 +33,7 @@ vtkmThreshold::Run(vtkm::cont::DataSet &input,
   thresholder.SetActiveField(field_name);
   thresholder.SetFieldsToPass(map_fields);
   auto output = thresholder.Execute(input);
-  //vtkh::StripPermutation(output);
+  
   return output;
 }
 
