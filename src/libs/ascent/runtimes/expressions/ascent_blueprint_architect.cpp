2428aedc487e9a033047d21ce2c84c95350b1fa6
Cyrus Harrison <cyrush@llnl.gov>
2023-07-24 14:27:43 -0700

    stop being picky about int32

--- src/libs/ascent/runtimes/expressions/ascent_blueprint_architect.cpp
+++ src/libs/ascent/runtimes/expressions/ascent_blueprint_architect.cpp
@@ -1234,7 +1234,7 @@ int find_bin(const T* bins, const int size, const T val, bool clamp)
 }
 
 // returns -1 if value lies outside the range
-int
+conduit::index_t
 get_bin_index(const conduit::float64 value, const conduit::Node &axis)
 {
   const bool clamp = axis["clamp"].to_uint8();
@@ -1250,8 +1250,8 @@ get_bin_index(const conduit::float64 value, const conduit::Node &axis)
       axis["num_bins"].to_float64() /
       (axis["max_val"].to_float64() - axis["min_val"].to_float64());
 
-  const int bin_index =
-      static_cast<int>((value - axis["min_val"].to_float64()) * inv_delta);
+  const conduit::index_t bin_index =
+      static_cast<conduit::index_t>((value - axis["min_val"].to_float64()) * inv_delta);
 
   if(clamp)
   {
@@ -1259,12 +1259,12 @@ get_bin_index(const conduit::float64 value, const conduit::Node &axis)
     {
       return 0;
     }
-    else if(bin_index >= axis["num_bins"].as_int32())
+    else if(bin_index >= axis["num_bins"].to_index_t())
     {
-      return axis["num_bins"].as_int32() - 1;
+      return axis["num_bins"].to_index_t() - 1;
     }
   }
-  else if(bin_index < 0 || bin_index >= axis["num_bins"].as_int32())
+  else if(bin_index < 0 || bin_index >= axis["num_bins"].to_index_t())
   {
     return -1;
   }
@@ -1396,7 +1396,7 @@ populate_homes(const conduit::Node &dom,
     if(bin_axes.child(axis_index).has_path("num_bins"))
     {
       // uniform axis
-      stride *= bin_axes.child(axis_index)["num_bins"].as_int32();
+      stride *= bin_axes.child(axis_index)["num_bins"].to_index_t();
     }
     else
     {
@@ -1540,7 +1540,7 @@ binning(const conduit::Node &dataset,
   int num_axes = bin_axes.number_of_children();
 
   // create bins
-  size_t num_bins = 1;
+  index_t num_bins = 1;
   for(int axis_index = 0; axis_index < num_axes; ++axis_index)
   {
     conduit::Node &axis = bin_axes.child(axis_index);
@@ -1548,18 +1548,18 @@ binning(const conduit::Node &dataset,
 
     if(!bin_axes.child(axis_index).has_child("min_val"))
     {
-      axis["min_val"] = field_min(dataset, axis_name)["value"].as_float64();
+      axis["min_val"] = field_min(dataset, axis_name)["value"].to_float64();
     }
     if(!axis.has_child("max_val"))
     {
       // TODO: FIXME THE BASELINES REQUIRE +1 here, not good.
-      axis["max_val"] = field_max(dataset, axis_name)["value"].as_float64() + 1.0;
+      axis["max_val"] = field_max(dataset, axis_name)["value"].to_float64() + 1.0;
     }
 
     if(bin_axes.child(axis_index).has_path("num_bins"))
     {
       // uniform axis
-      num_bins *= bin_axes.child(axis_index)["num_bins"].as_int32();
+      num_bins *= bin_axes.child(axis_index)["num_bins"].to_index_t();
     }
     else
     {
@@ -1986,7 +1986,7 @@ binning_mesh(const conduit::Node &binning,
     else
     {
       // uniform
-      const int dim = axis["num_bins"].as_int32() + 1;
+      const conduit::index_t dim = axis["num_bins"].to_index_t() + 1;
       const double delta =
           (axis["max_val"].to_float64() - axis["min_val"].to_float64()) /
           (dim - 1);
@@ -2496,17 +2496,17 @@ void paint_nestsets(const std::string nestset_name,
     ASCENT_ERROR("Paint nestsets: cannot paint on unstructured topology");
   }
 
-  int el_dims[3] = {1,1,1};
+  conduit::index_t el_dims[3] = {1,1,1};
   bool is_3d = false;
 
   if(topo["type"].as_string() == "structured")
   {
-    el_dims[0] = topo["elements/dims/i"].to_int32();
-    el_dims[1] = topo["elements/dims/j"].to_int32();
+    el_dims[0] = topo["elements/dims/i"].to_index_t();
+    el_dims[1] = topo["elements/dims/j"].to_index_t();
     if(topo.has_path("elements/dims/k"))
     {
       is_3d = true;
-      el_dims[2] = topo["elements/dims/k"].to_int32();
+      el_dims[2] = topo["elements/dims/k"].to_index_t();
     }
 
   }
@@ -2516,13 +2516,13 @@ void paint_nestsets(const std::string nestset_name,
     const conduit::Node &coords = dom["coordsets/"+coord_name];
     if(coords["type"].as_string() == "uniform")
     {
-      el_dims[0] = coords["dims/i"].as_int32() - 1;
-      el_dims[1] = coords["dims/j"].as_int32() - 1;
+      el_dims[0] = coords["dims/i"].to_index_t() - 1;
+      el_dims[1] = coords["dims/j"].to_index_t() - 1;
 
       if(coords.has_path("dims/k"))
       {
         is_3d = true;
-        el_dims[2] = coords["dims/k"].to_int32() -1;
+        el_dims[2] = coords["dims/k"].to_index_t() -1;
       }
     }
     else if(coords["type"].as_string() == "rectilinear")
@@ -2541,13 +2541,13 @@ void paint_nestsets(const std::string nestset_name,
     }
   }
 
-  conduit::int32 field_size = el_dims[0] * el_dims[1];
+  conduit::index_t field_size = el_dims[0] * el_dims[1];
   if(is_3d)
   {
     field_size *= el_dims[2];
   }
 
-  conduit::int32_array levels;
+  conduit::index_t_array levels;
   // check to see if the field already has data or if
   // we need to create a new field
   if(field.has_path("values"))
@@ -2564,7 +2564,7 @@ void paint_nestsets(const std::string nestset_name,
   {
     field["association"] = "element";
     field["topology"] = topo_name;
-    field["values"] = conduit::DataType::int32(field_size);
+    field["values"] = conduit::DataType::index_t(field_size);
     levels = field["values"].value();
     for(int i = 0; i < field_size; ++i)
     {
@@ -2577,8 +2577,8 @@ void paint_nestsets(const std::string nestset_name,
   // across all domains
   if(!dom.has_path("nestsets/"+nestset_name))
   {
-    // we alrady init'd it to zero if its a new field, so we are good to go
-    // if it wasnt' a new field, it should already have the correct values
+    // we already init'd it to zero if its a new field, so we are good to go
+    // if it wasn't' a new field, it should already have the correct values
     return;
   }
 
@@ -2595,36 +2595,36 @@ void paint_nestsets(const std::string nestset_name,
       continue;
     }
 
-    int origin[3];
-    origin[0] = window["origin/i"].to_int32();
-    origin[1] = window["origin/j"].to_int32();
+    conduit::index_t origin[3];
+    origin[0] = window["origin/i"].to_index_t();
+    origin[1] = window["origin/j"].to_index_t();
 
     if(is_3d)
     {
-      origin[2] = window["origin/k"].to_int32();
+      origin[2] = window["origin/k"].to_index_t();
     }
 
-    int dims[3];
-    dims[0] = window["dims/i"].to_int32();
-    dims[1] = window["dims/j"].to_int32();
+    conduit::index_t dims[3];
+    dims[0] = window["dims/i"].to_index_t();
+    dims[1] = window["dims/j"].to_index_t();
     if(is_3d)
     {
-      dims[2] = window["dims/k"].to_int32();
+      dims[2] = window["dims/k"].to_index_t();
     }
     if(is_3d)
     {
       // all the nesting relationship is local
-      for(int z = origin[2]; z < origin[2] + dims[2]; ++z)
+      for(index_t z = origin[2]; z < origin[2] + dims[2]; ++z)
       {
-        const int z_offset = z * el_dims[0] * el_dims[1];
+        const conduit::index_t z_offset = z * el_dims[0] * el_dims[1];
         for(int y = origin[1]; y < origin[1] + dims[1]; ++y)
         {
-          const conduit::int32 y_offset = y * el_dims[0];
+          const conduit::index_t y_offset = y * el_dims[0];
           for(int x = origin[0]; x < origin[0] + dims[0]; ++x)
           {
             // this might a ghost field, but we only want to
             // mask real zones that are masked by finer grids
-            conduit::int32 value = levels[z_offset + y_offset + x];
+            conduit::index_t value = levels[z_offset + y_offset + x];
             if(value == 0)
             {
               levels[z_offset + y_offset + x] = 1;
@@ -2636,14 +2636,14 @@ void paint_nestsets(const std::string nestset_name,
     else
     {
       // all the nesting relationship is local
-      for(int y = origin[1]; y < origin[1] + dims[1]; ++y)
+      for(conduit::index_t y = origin[1]; y < origin[1] + dims[1]; ++y)
       {
-        const conduit::int32 y_offset = y * el_dims[0];
-        for(int x = origin[0]; x < origin[0] + dims[0]; ++x)
+        const conduit::index_t y_offset = y * el_dims[0];
+        for(conduit::index_t x = origin[0]; x < origin[0] + dims[0]; ++x)
         {
           // this might a ghost field, but we only want to
           // mask real zones that are masked by finer grids
-          conduit::int32 value = levels[y_offset + x];
+          conduit::index_t value = levels[y_offset + x];
           if(value == 0)
           {
             levels[y_offset + x] = 1;
