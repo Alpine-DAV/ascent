701f9490a36855629a25ecff9a6b1ca62c9b78fb
Nicole Marsaglia <marsaglia1@llnl.gov>
2023-09-12 14:40:50 -0400

    changes made. let's see what breaks

--- src/libs/ascent/runtimes/ascent_main_runtime.cpp
+++ src/libs/ascent/runtimes/ascent_main_runtime.cpp
@@ -653,7 +653,7 @@ AscentRuntime::EnsureDomainIds()
     }
 #ifdef ASCENT_MPI_ENABLED
     conduit::Node n_ids;
-    n_ids.set(int[comm_size]);
+    n_ids.set(DataType::c_int(comm_size));
     int *unique_ids_array = n_ids.value();//new int[comm_size];
     MPI_Allgather(&unique_ids, 1, MPI_INT, unique_ids_array, 1, MPI_INT, mpi_comm);
     for(int i = 0; i < comm_size; i++)
@@ -671,10 +671,18 @@ AscentRuntime::EnsureDomainIds()
 
 #ifdef ASCENT_MPI_ENABLED
 
-    int *domain_ids_per_rank = new int[total_domains];
-    int *global_domain_ids = new int[total_domains];
-    int *domain_rank = new int[total_domains];
-    int *global_domain_rank = new int[total_domains];
+    conduit::Node n_dom_ids;
+    conduit::Node n_global_dom_ids;
+    conduit::Node n_dom_rank;
+    conduit::Node n_global_dom_rank;
+    n_dom_ids.set(DataType::c_int(total_domains));
+    n_global_dom_ids.set(DataType::c_int(total_domains));
+    n_dom_rank.set(DataType::c_int(total_domains));
+    n_global_dom_rank.set(DataType::c_int(total_domains));
+    int *domain_ids_per_rank = n_dom_ids.value();
+    int *global_domain_ids = n_global_dom_ids.value();
+    int *domain_rank = n_dom_rank.value();
+    int *global_domain_rank = n_global_dom_rank.value();
 
     for(int i = 0; i < total_domains; i++)
     {
@@ -697,22 +705,17 @@ AscentRuntime::EnsureDomainIds()
       global_ids.insert(pair<int,int>(global_domain_ids[i],global_domain_rank[i]));
     }
     
-    delete[] domain_ids_per_rank;
-    delete[] global_domain_ids;
-    delete[] domain_rank;
-    delete[] global_domain_rank;
- 
     if(global_ids.size() != total_domains)
     {
       std::multimap<int,int>::iterator itr;
       std::stringstream ss;
       for(itr = global_ids.begin(); itr != global_ids.end(); ++itr)
       {
-        if(map.count(itr.first)>1)
+        if(global_ids.count(itr->first)>1)
 	{
 	  std::pair <std::multimap<int,int>::iterator, std::multimap<int,int>::iterator> list;
-	  list = global_ids.equal_range(itr.first);
-	  ss << "domain: " << itr.first << " on ranks: ";
+	  list = global_ids.equal_range(itr->first);
+	  ss << "domain: " << itr->first << " on ranks: ";
 	  for(std::multimap<int,int>::iterator it=list.first; it!=list.second; ++it)
 	  {
             ss << it->second << " ";
