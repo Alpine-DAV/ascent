26a7bd84241f39f6e96f628ebafb0eedea5d79f0
Cyrus Harrison <cyrush@llnl.gov>
2023-11-06 15:43:14 -0800

    update

--- src/tests/thirdparty/t_zlib_smoke.cpp
+++ src/tests/thirdparty/t_zlib_smoke.cpp
@@ -16,6 +16,9 @@
 //-----------------------------------------------------------------------------
 TEST(zlib_smoke, zlib_basic)
 {
+
+  // exercise compress and uncompress interface
+
   Bytef compress_dest[32];
   Bytef compress_src[32];
   Bytef uncompress_dest[64];
@@ -50,36 +53,5 @@ TEST(zlib_smoke, zlib_basic)
   {
     EXPECT_EQ(compress_src[i],uncompress_dest[i]);
   }
-
-  //
-  // EXTERN int ZEXPORT compress OF((Bytef *dest,   uLongf *destLen, const Bytef *source, uLong sourceLen));
-  // /*
-  //          Compresses the source buffer into the destination buffer.  sourceLen is
-  //      the byte length of the source buffer.  Upon entry, destLen is the total size
-  //      of the destination buffer, which must be at least the value returned by
-  //      compressBound(sourceLen).  Upon exit, destLen is the actual size of the
-  //      compressed buffer.
-  //
-  //          compress returns Z_OK if success, Z_MEM_ERROR if there was not
-  //      enough memory, Z_BUF_ERROR if there was not enough room in the output
-  //      buffer.
-  // */
-  //
-  // ZEXTERN int ZEXPORT uncompress OF((Bytef *dest,   uLongf *destLen, const Bytef *source, uLong sourceLen));
-  // /*
-  //          Decompresses the source buffer into the destination buffer.  sourceLen is
-  //      the byte length of the source buffer.  Upon entry, destLen is the total size
-  //      of the destination buffer, which must be large enough to hold the entire
-  //      uncompressed data.  (The size of the uncompressed data must have been saved
-  //      previously by the compressor and transmitted to the decompressor by some
-  //      mechanism outside the scope of this compression library.) Upon exit, destLen
-  //      is the actual size of the uncompressed buffer.
-  //
-  //          uncompress returns Z_OK if success, Z_MEM_ERROR if there was not
-  //      enough memory, Z_BUF_ERROR if there was not enough room in the output
-  //      buffer, or Z_DATA_ERROR if the input data was corrupted or incomplete.  In
-  //      the case where there is not enough room, uncompress() will fill the output
-  //      buffer with the uncompressed data up to that point.
-  // */
 }
 
