521b6919f3ffab99ce97bc0e69faf2743434f968
Victor Mateevitsi <vmateevitsi@anl.gov>
2024-01-10 20:47:18 +0000

    Polaris install script fix

--- scripts/build_ascent/build_ascent_cuda_polaris.sh
+++ scripts/build_ascent/build_ascent_cuda_polaris.sh
@@ -1,11 +1,13 @@
+#!/bin/bash -l
 module load cmake
 
 # Swap NVHPC with GNU compilers
 module swap PrgEnv-nvhpc PrgEnv-gnu
+module load gcc/11.2.0
 module load cudatoolkit-standalone
 
 export CC=$(which cc)
 export CXX=$(which CC)
 export FTN=$(which ftn)
 
-env enable_mpi=ON enable_fortran=ON raja_enable_vectorization=OFF ./build_ascent_cuda.sh
+env enable_mpi=ON enable_fortran=ON raja_enable_vectorization=OFF enable_tests=OFF ./build_ascent_cuda.sh
