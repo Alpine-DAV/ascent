3863917ca559fe9ad7210b377c1ebaac79584227
Qi Wu <qwu@anl.gov>
2024-03-19 00:48:26 +0000

    update script

--- scripts/build_ascent/build_ascent_cuda_redbull.sh
+++ scripts/build_ascent/build_ascent_cuda_redbull.sh
@@ -1,16 +1,7 @@
-# module load cmake
-# # Swap NVHPC with GNU compilers
-# module swap PrgEnv-nvhpc PrgEnv-gnu
-# module load cudatoolkit-standalone
+#!/bin/bash -l
 
-# export http_proxy="http://proxy-01.pub.alcf.anl.gov:3128"
-# export https_proxy="http://proxy-01.pub.alcf.anl.gov:3128"
-# export ftp_proxy="http://proxy-01.pub.alcf.anl.gov:3128"
+export CC=$(which cc)
+export CXX=$(which CC)
+export FTN=$(which ftn)
 
-# source /home/qiwu/projects/diva_superbuild/setup-env.sh
-
-export CC=$(which gcc)
-export CXX=$(which g++)
-export FTN=$(which gfortran)
-
-env enable_mpi=ON enable_fortran=ON raja_enable_vectorization=OFF build_ascent=false CUDA_ARCH=75 ./build_ascent_cuda.sh
+env enable_mpi=ON enable_fortran=ON raja_enable_vectorization=OFF enable_tests=OFF build_ascent=false ./build_ascent_cuda.sh
