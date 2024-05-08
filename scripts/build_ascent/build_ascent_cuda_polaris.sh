32d08487eca2122b164d0c1a593a818210e65ac4
Victor Mateevitsi <mvictoras@gmail.com>
2024-05-08 11:15:19 -0500

    Polaris fix (#1284)
    Co-authored-by: Victor <mvictoras@sirius-uan-0002.hsn.cm.sirius.alcf.anl.gov>

--- scripts/build_ascent/build_ascent_cuda_polaris.sh
+++ scripts/build_ascent/build_ascent_cuda_polaris.sh
@@ -1,13 +1,16 @@
 #!/bin/bash -l
-module load cmake
-
-# Swap NVHPC with GNU compilers
-module swap PrgEnv-nvhpc PrgEnv-gnu
-module load gcc/11.2.0
+module use /soft/modulefiles
+module load PrgEnv-gnu
+module load nvhpc-mixed
+module load craype-accel-nvidia80
+module unload nvhpc-mixed
+module load spack-pe-base cmake
 module load cudatoolkit-standalone
+module load craype-x86-milan
+module load cray-python
 
 export CC=$(which cc)
 export CXX=$(which CC)
 export FTN=$(which ftn)
 
-env enable_mpi=ON enable_fortran=ON raja_enable_vectorization=OFF enable_tests=OFF ./build_ascent_cuda.sh
+env enable_python=ON enable_mpi=ON enable_fortran=ON raja_enable_vectorization=OFF enable_tests=OFF ./build_ascent_cuda.sh
