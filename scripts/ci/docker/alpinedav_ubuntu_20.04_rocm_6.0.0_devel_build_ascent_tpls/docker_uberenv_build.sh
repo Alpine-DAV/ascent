395ced8e54894942a0e51cd38217c51ac4ee9106
Cyrus Harrison <cyrush@llnl.gov>
2024-07-15 14:12:05 -0700

    new docker build with clang asan build recipe

--- scripts/ci/docker/alpinedav_ubuntu_20.04_rocm_6.0.0_devel_build_ascent_tpls/docker_uberenv_build.sh
+++ scripts/ci/docker/alpinedav_ubuntu_20.04_rocm_6.0.0_devel_build_ascent_tpls/docker_uberenv_build.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 set -ev
 
-cmake_install_dir=/cmake-3.23.2-linux-x86_64
+export CMAKE_ARCH=`uname -m`
+
+if [[ ${CMAKE_ARCH} == "arm64" ]]; then
+  export CMAKE_ARCH="aarch64"
+fi
+
+cmake_install_dir=/cmake-3.23.2-linux-${CMAKE_ARCH}
 if [ ! -d ${cmake_install_dir} ]; then
-  echo "**** Downloading ${hdf5_tarball}"
   # setup cmake in container
-  curl -L https://github.com/Kitware/CMake/releases/download/v3.23.2/cmake-3.23.2-linux-x86_64.tar.gz -o cmake-3.23.2-linux-x86_64.tar.gz
-  tar -xzf cmake-3.23.2-linux-x86_64.tar.gz
+  curl -L https://github.com/Kitware/CMake/releases/download/v3.23.2/cmake-3.23.2-linux-${CMAKE_ARCH}.tar.gz -o cmake-3.23.2-linux-${CMAKE_ARCH}.tar.gz
+  tar -xzf cmake-3.23.2-linux-${CMAKE_ARCH}.tar.gz
 fi
 
 export PATH=$PATH:/${cmake_install_dir}/bin/
