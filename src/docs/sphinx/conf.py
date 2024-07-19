a0665afe160f716f261d7cf72385bf5db3a08492
Cyrus Harrison <cyrush@llnl.gov>
2024-07-19 08:32:24 -0700

    rtd config: add jquery to req extensions (#1334)

--- src/docs/sphinx/conf.py
+++ src/docs/sphinx/conf.py
@@ -38,6 +38,7 @@ import os
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
+    'sphinxcontrib.jquery',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax'
