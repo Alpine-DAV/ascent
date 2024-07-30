9ae5989c9462c966dcd923dfffccc09f79bbc552
Cyrus Harrison <cyrush@llnl.gov>
2024-07-29 19:43:15 -0700

    docker recipe adjustments

--- src/examples/docker/ubuntu-build-ascent/docker_build_ascent.sh
+++ src/examples/docker/ubuntu-build-ascent/docker_build_ascent.sh
@@ -3,19 +3,19 @@ set -ev
 
 env enable_python=ON \
     enable_mpi=ON \
+    enable_FORTRAN=ON \
     enable_openmp=ON \
     build_pyvenv=true \
     /ascent/scripts/build_ascent/build_ascent.sh
 
-# ascent is installed at /install/ascent-checkout
-ln -s /install/ascent-checkout/ /ascent/install
-
-# install extra packages
+# install extra python modules
 /install/python-venv/bin/python3 -m pip install \
     --trusted-host pypi.org --trusted-host files.pythonhosted.org \
     bash_kernel \
     jupyter \
     jupyterlab \
+    ipykernel \
+    pylint \
     ipywidgets \
     ipympl \
     matplotlib \
@@ -26,16 +26,8 @@ ln -s /install/ascent-checkout/ /ascent/install
     h5py \
     llnl-hatchet
 
-
-# install ascent bridge kernel into jupyter
-cd /install/ascent-checkout/share/ascent/ascent_jupyter_bridge/ && \
-    /install/python-venv/bin/python3 -m pip install \
-    --trusted-host pypi.org --trusted-host files.pythonhosted.org \
-    -r requirements.txt
-
 cd /install/ascent-checkout/share/ascent/ascent_jupyter_bridge/ && \
     /install/python-venv/bin/python3 -m ipykernel install
 
-
 echo "export PATH=/install/python-venv/bin/:$PATH" >> /ascent_docker_setup_env.sh
-echo "export PYTHONPATH=/ascent/install/python-modules/" >> /ascent_docker_setup_env.sh
+echo "export PYTHONPATH=/install/ascent-checkout/python-modules/" >> /ascent_docker_setup_env.sh
