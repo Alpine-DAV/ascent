a67922a3148e0d9508a1c62d3fe7f63b7c3bec79
Cyrus <cyrush@llnl.gov>
2024-08-13 16:26:04 -0700

    update rzvernal recipe

--- scripts/build_ascent/build_ascent_hip_rzvernal.sh
+++ scripts/build_ascent/build_ascent_hip_rzvernal.sh
@@ -1,16 +1,15 @@
 module load cmake/3.24.2
 module load craype-accel-amd-gfx90a
-module load rocm/6.1.2
-module load cray-mpich
+module load rocmcc/6.1.2-magic
 
 export MPICH_GPU_SUPPORT_ENABLED=1
 export ROCM_ARCH=gfx90a
 
 export CC=/opt/rocm-6.1.2/bin/amdclang
 export CXX=/opt/rocm-6.1.2/bin/amdclang++
-export CFLAGS="-I${ROCM_PATH}/include"
-export CXXFLAGS="-I${ROCM_PATH}/include -Wno-pass-failed"
-export LDFLAGS="-L${ROCM_PATH}/lib -lamdhip64"
+#export CFLAGS="-I${ROCM_PATH}/include"
+#export CXXFLAGS="-I${ROCM_PATH}/include -Wno-pass-failed"
+#export LDFLAGS="-L${ROCM_PATH}/lib -lamdhip64"
 
 export enable_python="${enable_python:=ON}"
 export build_caliper="${build_caliper:=true}"
