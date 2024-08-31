b04a30de88b51afc22a05409612975cdb6218336
Nicole Marsaglia <marsaglia1@llnl.gov>
2024-08-30 17:01:32 -0700

    errors on errors

--- src/libs/ascent/runtimes/ascent_vtkh_device_utils.hpp
+++ src/libs/ascent/runtimes/ascent_vtkh_device_utils.hpp
@@ -38,61 +38,62 @@ public:
 //---------------------------------------------------------------------------//
 // CUDA decorators
 //---------------------------------------------------------------------------//
-std::string memory_space = "device";
+static const std::string memory_space = "device";
 #define ASCENT_EXEC inline __host__ __device__
 // Note: there is a performance hit for doing both host and device
 // the cuda compiler calls this on then host as a std::function call for each i
 // in the for loop, and that basically works out to a virtual function
 // call. Thus for small loops, the know overhead is about 3x
 #define ASCENT_LAMBDA __device__ __host__
-#if defined(ASCENT_RAJA_ENABLED)
-#define BLOCK_SIZE 128
-using for_policy = RAJA::cuda_exec<BLOCK_SIZE>;
-using reduce_policy = RAJA::cuda_reduce;
-using atomic_policy = RAJA::cuda_atomic;
-#else
-using for_policy    = EmptyPolicy;
-using reduce_policy = EmptyPolicy;
-using atomic_policy = EmptyPolicy;
-#endif
+//#if defined(ASCENT_RAJA_ENABLED)
+//#define BLOCK_SIZE 128
+//using for_policy = RAJA::cuda_exec<BLOCK_SIZE>;
+//using reduce_policy = RAJA::cuda_reduce;
+//using atomic_policy = RAJA::cuda_atomic;
+//#else
+//using for_policy    = EmptyPolicy;
+//using reduce_policy = EmptyPolicy;
+//using atomic_policy = EmptyPolicy;
+//#endif
 
 #elif defined(ASCENT_HIP_ENABLED) // && ?
 //---------------------------------------------------------------------------//
 // HIP decorators
 //---------------------------------------------------------------------------//
-std::string memory_space = "device";
+static const std::string memory_space = "device";
 #define ASCENT_EXEC inline __host__ __device__
 #define ASCENT_LAMBDA __device__ __host__
-#if defined(ASCENT_RAJA_ENABLED)
-#define BLOCK_SIZE 256
-using for_policy = RAJA::hip_exec<BLOCK_SIZE>;
-using reduce_policy = RAJA::hip_reduce;
-using atomic_policy = RAJA::hip_atomic;
-#else
-using for_policy    = EmptyPolicy;
-using reduce_policy = EmptyPolicy;
-using atomic_policy = EmptyPolicy;
-#endif
+//#if defined(ASCENT_RAJA_ENABLED)
+//#define BLOCK_SIZE 256
+//using for_policy = RAJA::hip_exec<BLOCK_SIZE>;
+//using reduce_policy = RAJA::hip_reduce;
+//using atomic_policy = RAJA::hip_atomic;
+//#else
+//using for_policy    = EmptyPolicy;
+//using reduce_policy = EmptyPolicy;
+//using atomic_policy = EmptyPolicy;
+//#endif
 
 #else
 //---------------------------------------------------------------------------//
 // Non-device decorators
 //---------------------------------------------------------------------------//
-std::string memory_space = "host";
+static const std::string memory_space = "host";
 #define ASCENT_EXEC   inline
 #define ASCENT_LAMBDA
-#if defined(ASCENT_RAJA_ENABLED)
-using for_policy = RAJA::seq_exec;
-using reduce_policy = RAJA::seq_reduce;
-using atomic_policy = RAJA::seq_atomic;
-#else
-using for_policy    = EmptyPolicy;
-using reduce_policy = EmptyPolicy;
-using atomic_policy = EmptyPolicy;
+//#if defined(ASCENT_RAJA_ENABLED)
+//using for_policy = RAJA::seq_exec;
+//using reduce_policy = RAJA::seq_reduce;
+//using atomic_policy = RAJA::seq_atomic;
+//#else
+//using for_policy    = EmptyPolicy;
+//using reduce_policy = EmptyPolicy;
+//using atomic_policy = EmptyPolicy;
+//#endif
 #endif
 
 // Definition of the cast function with __host__ __device__
-ASCENT_EXEC void castUint64ToFloat64(const uint64_t* input, double* output, int size);
+ASCENT_EXEC static void castUint64ToFloat64(const uint64_t* input, double* output, int size);
 
 
 };
