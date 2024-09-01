5d3b6dd80e7a2472440b65d0cf557566e997dd70
WU Qi <wilson.over.cloud@gmail.com>
2024-08-31 21:56:28 -0700

    update scripts

--- scripts/build_ascent/build_ascent_cuda_redbull.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -l
-
-export CC=$(which cc)
-export CXX=$(which CC)
-export FTN=$(which ftn)
-
-env enable_mpi=ON enable_fortran=ON raja_enable_vectorization=OFF enable_tests=OFF build_ascent=false ./build_ascent_cuda.sh
