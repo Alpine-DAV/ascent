78c72ce6a9fd459e47d53d5758fb57f98aec72eb
Emily Howell <howell31@llnl.gov>
2025-03-20 09:18:22 -0700

    moving zoom check to a more logical location

--- src/libs/ascent/runtimes/flow_filters/ascent_runtime_rendering_filters.cpp
+++ src/libs/ascent/runtimes/flow_filters/ascent_runtime_rendering_filters.cpp
@@ -223,13 +223,6 @@ check_renders_surprises(const conduit::Node &renders_node)
         }
       }
     }
-
-    if(render_node.has_path("camera/zoom")){
-        float zoom_val = render_node["camera/zoom"].to_value();
-        if (zoom_val <= 0) {
-            ASCENT_ERROR("Zoom must be greater than zero. Default zoom is 1.\n");
-        }
-    }
   }
   return surprises;
 }
@@ -766,7 +759,12 @@ public:
     conduit::Node zoom;
     if(render_copy.has_path("camera/zoom"))
     {
-      zoom = render_copy["camera/zoom"];
+        float zoom_val = render_node["camera/zoom"].to_value();
+        if (zoom_val <= 0) {
+            ASCENT_ERROR("Zoom must be greater than zero. Default zoom is 1.\n");
+        }
+
+        zoom = render_copy["camera/zoom"];
     }
 
     // cinema is controlling the camera so get
