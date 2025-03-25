0c322e25517d66011ea48985d0cc86017f424442
Cyrus Harrison <cyrush@llnl.gov>
2025-03-25 11:27:21 -0700

    windows fix

--- scripts/build_ascent/build_ascent.sh
+++ scripts/build_ascent/build_ascent.sh
@@ -515,6 +515,7 @@ if [ ! -d ${conduit_src_dir} ]; then
 
   patch -p1 < ${script_dir}/2025_03_21_conduit_pr1370.patch
   patch -p1 < ${script_dir}/2025_03_conduit_windows_symbol_export_fix.patch
+  patch -p1 < ${script_dir}/2025_03_25_conduit_hdf5_win_detect_fix.patch
 
   cd ${root_dir}
 fi
