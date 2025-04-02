451b43427d872d6811314fe2ce48271f74357c39
Cyrus Harrison <cyrush@llnl.gov>
2025-04-02 14:28:28 -0700

    solve issue with python vs silo static builds

--- scripts/ci/docker/alpinedav_ubuntu_22.04_cuda_11.8.0_devel_tpls/docker_uberenv_build.sh
+++ scripts/ci/docker/alpinedav_ubuntu_22.04_cuda_11.8.0_devel_tpls/docker_uberenv_build.sh
@@ -4,9 +4,10 @@ set -ev
 # variants
 # TODO:
 # (+genten) genten currently disabled, wait for genten master to gain cokurt
-export SPACK_SPEC="%gcc+mpi+cuda+vtkh+dray+mfem+occa~genten~python~openmp~shared cuda_arch=70"
+export SPACK_SPEC="%gcc+mpi+cuda+vtkh+dray+mfem+occa~python~openmp~shared cuda_arch=70"
 # constraints
-export SPACK_SPEC="${SPACK_SPEC} ^raja+cuda~openmp cuda_arch=70 ^umpire+cuda~openmp cuda_arch=70 ^camp+cuda~openmp cuda_arch=70 ^hdf5~mpi ^mfem~cuda~petsc~sundials~slepc ^hypre~cuda ^vtk-m+cuda~kokkos cuda_arch=70"
+# note: silo static build with spack will fail unless you disable python
+export SPACK_SPEC="${SPACK_SPEC} ^raja+cuda~openmp cuda_arch=70 ^umpire+cuda~openmp cuda_arch=70 ^camp+cuda~openmp cuda_arch=70 ^hdf5~mpi ^mfem~cuda~petsc~sundials~slepc ^hypre~cuda ^vtk-m+cuda~kokkos cuda_arch=70 ^silo~python~mpi"
 # config
 export SPACK_ENV_YAML="scripts/uberenv_configs/spack_configs/envs/alpinedav-ubuntu_20.04_cuda_11.4.3_devel/spack.yaml"
 
@@ -18,7 +19,7 @@ cd ascent && python3 scripts/uberenv/uberenv.py \
      --prefix="/uberenv_libs"
 
 # cleanup the spack build stuff to free up space
-/uberenv_libs/spack/bin/spack clean --all 
+/uberenv_libs/spack/bin/spack clean --all
 
 # change perms
 chmod -R a+rX /uberenv_libs
