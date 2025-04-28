4d5a877f17842818573cd1175576ae4f061cb2bd
Cyrus Harrison <cyrush@llnl.gov>
2025-04-28 09:38:13 -0700

    build_ascent and ci updates

--- scripts/build_ascent/build_ascent.sh
+++ scripts/build_ascent/build_ascent.sh
@@ -510,14 +510,6 @@ if [ ! -d ${conduit_src_dir} ]; then
   tar ${tar_extra_args} -xzf ${conduit_tarball} -C ${source_dir} \
       --exclude="conduit-${conduit_version}/src/tests/relay/data/silo/*"
 
-  # apply patches
-  cd  ${conduit_src_dir}
-
-  patch -p1 < ${script_dir}/2025_03_21_conduit_pr1370.patch
-  patch -p1 < ${script_dir}/2025_03_conduit_windows_symbol_export_fix.patch
-  patch -p1 < ${script_dir}/2025_03_25_conduit_hdf5_win_detect_fix.patch
-
-  cd ${root_dir}
 fi
 
 #
