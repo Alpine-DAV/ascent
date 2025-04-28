957bcd1ea280a9d31d9e57d2eb43aa6049a77aac
Cyrus Harrison <cyrush@llnl.gov>
2025-04-28 15:46:37 -0700

    update raja

--- scripts/build_ascent/build_ascent.sh
+++ scripts/build_ascent/build_ascent.sh
@@ -775,7 +775,7 @@ fi # build_camp
 ################
 # RAJA
 ################
-raja_version=v2025.03.0
+raja_version=v2025.03.1
 raja_src_dir=$(ospath ${source_dir}/RAJA-${raja_version})
 raja_build_dir=$(ospath ${build_dir}/raja-${raja_version})
 raja_install_dir=$(ospath ${install_dir}/raja-${raja_version}/)
