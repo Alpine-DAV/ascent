2303e1dbf7a46681dff1bb3262a02c0aaa96039b
Justin Privitera <privitera1@llnl.gov>
2025-05-13 10:51:58 -0700

    working (if mostly useless) test

--- src/tests/ascent/t_ascent_rover.cpp
+++ src/tests/ascent/t_ascent_rover.cpp
@@ -19,6 +19,7 @@
 #include <math.h>
 
 #include <conduit_blueprint.hpp>
+#include <conduit_relay.hpp>
 
 #include "t_config.hpp"
 #include "t_utils.hpp"
@@ -31,7 +32,48 @@ using namespace conduit;
 using namespace ascent;
 
 
-index_t EXAMPLE_MESH_SIDE_DIM = 20;
+constexpr index_t EXAMPLE_MESH_SIDE_DIM = 20;
+
+std::string render_blueprint_result(const std::string &field_name,
+                                    const std::string &output_image_file_basename,
+                                    const Node &data)
+{
+    const std::string output_path = prepare_output_dir();
+    const std::string output_file = 
+        conduit::utils::join_file_path(output_path, output_image_file_basename);
+
+    //
+    // Create the actions.
+    //
+
+    conduit::Node scenes;
+    scenes["s1/plots/p1/type"] = "pseudocolor";
+    scenes["s1/plots/p1/field"] = field_name;
+    scenes["s1/image_prefix"] = output_file;
+
+    conduit::Node actions;
+    conduit::Node &add_plots = actions.append();
+    add_plots["action"] = "add_scenes";
+    add_plots["scenes"] = scenes;
+
+    //
+    // Run Ascent
+    //
+
+    Ascent ascent;
+
+    Node ascent_opts;
+    // ascent_opts["ascent_info"] = "verbose";
+    ascent_opts["timings"] = "true";
+    ascent_opts["runtime/type"] = "ascent";
+    ascent.open(ascent_opts);
+    ascent.publish(data);
+    ascent.execute(actions);
+    ascent.close();
+
+    return output_file;
+}
+
 
 #if 0
 //-----------------------------------------------------------------------------
@@ -354,9 +396,8 @@ TEST(ascent_rover, test_xray_blueprint)
 
     ASCENT_INFO("Testing xray_extract");
 
-
-    string output_path = prepare_output_dir();
-    string output_file = conduit::utils::join_file_path(output_path,"tout_rover_xray");
+    const std::string output_path = prepare_output_dir();
+    const std::string output_file = conduit::utils::join_file_path(output_path, "tout_rover_xray");
 
     // remove old images before rendering
     remove_test_image(output_file);
@@ -392,4 +433,85 @@ TEST(ascent_rover, test_xray_blueprint)
     ascent.publish(data);
     ascent.execute(actions);
     ascent.close();
+
+    const std::string full_outfile_name = output_file + "100.cycle_000100.root";
+
+    Node load_mesh;
+    conduit::relay::io::blueprint::load_mesh(full_outfile_name, load_mesh);
+    EXPECT_TRUE(conduit::blueprint::mesh::verify(load_mesh, verify_info));
+
+    const std::string out_image_name = 
+        render_blueprint_result("intensities", "tout_rover_xray_blueprint", load_mesh);
+
+    // TODO we need the render to make an interesting picture. This will be accomplished
+    // by working on the basic mesh output and changing the order of the dimensions.
+    // TODO we need a baseline in order for this to pass.
+    EXPECT_TRUE(check_test_image(out_image_name,0.01f));
+    std::string msg = "TODO we need a good description here";
+    ASCENT_ACTIONS_DUMP(actions, out_image_name, msg);
 }
+
+// //-----------------------------------------------------------------------------
+// TEST(ascent_rover, test_xray_blueprint_multi_curv3d)
+// {
+//     // the vtkm runtime is currently our only rendering runtime
+//     Node n;
+//     ascent::about(n);
+//     // only run this test if ascent was built with vtkm support
+//     if(n["runtimes/ascent/vtkm/status"].as_string() == "disabled")
+//     {
+//         ASCENT_INFO("Ascent support disabled, skipping test");
+//         return;
+//     }
+
+//     //
+//     // Create an example mesh.
+//     //
+//     Node data, verify_info;
+
+//     const std::string root_file = conduit::utils::join_file_path(std::string(ASCENT_T_DATA_DIR),
+//                                                                  "multi_curv3d_blueprint.cycle_000048.root");
+//     conduit::relay::io::blueprint::load_mesh(root_file, data);
+
+//     EXPECT_TRUE(conduit::blueprint::mesh::verify(data,verify_info));
+
+//     ASCENT_INFO("Testing xray_extract");
+
+//     string output_path = prepare_output_dir();
+//     string output_file = conduit::utils::join_file_path(output_path,"multi_curv3d_blueprint_xray");
+
+//     // remove old images before rendering
+//     remove_test_image(output_file);
+
+
+//     //
+//     // Create the actions.
+//     //
+
+//     conduit::Node extracts;
+//     extracts["e1/type"]  = "xray";
+//     // populate some param examples
+//     extracts["e1/params/absorption"] = "d";
+//     extracts["e1/params/emission"] = "p";
+//     extracts["e1/params/filename"] = output_file;
+//     extracts["e1/params/blueprint"] = "json";
+
+//     conduit::Node actions;
+//     // add the pipeline
+//     conduit::Node &add_extracts = actions.append();
+//     add_extracts["action"] = "add_extracts";
+//     add_extracts["extracts"] = extracts;
+
+//     //
+//     // Run Ascent
+//     //
+
+//     Ascent ascent;
+
+//     Node ascent_opts;
+//     ascent_opts["runtime/type"] = "ascent";
+//     ascent.open(ascent_opts);
+//     ascent.publish(data);
+//     ascent.execute(actions);
+//     ascent.close();
+// }
