01395f92b292f40a40d9f7648911faebb73cb418
Andres Sewell <sewellandres@gmail.com>
2025-06-30 14:19:07 -0700

    Try adding a single new file

---
+++ src/libs/vtkh/rendering/PartialComposite.hpp
@@ -0,0 +1,38 @@
+//============================================================================
+//  Copyright (c) Kitware, Inc.
+//  All rights reserved.
+//  See LICENSE.txt for details.
+//
+//  This software is distributed WITHOUT ANY WARRANTY; without even
+//  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+//  PURPOSE.  See the above copyright notice for more information.
+//============================================================================
+#ifndef vtkh_rendering_raytracing_PartialComposite_h
+#define vtkh_rendering_raytracing_PartialComposite_h
+
+#include <vtkh/vtkh_exports.h>
+
+#include <vtkm/cont/ArrayHandle.h>
+#include <vtkm/rendering/raytracing/ChannelBuffer.h>
+
+namespace vtkh
+{
+namespace rendering
+{
+namespace raytracing
+{
+
+template <typename FloatType>
+struct VTKH_API PartialComposite
+{
+  vtkm::cont::ArrayHandle<vtkm::Id> PixelIds;   // pixel that owns composite
+  vtkm::cont::ArrayHandle<FloatType> Distances; // distance of composite end
+  vtkm::rendering::raytracing::ChannelBuffer<FloatType> Buffer;              // holds either color or absorption
+  vtkm::rendering::raytracing::ChannelBuffer<FloatType> Intensities;           // holds the intensity emerging from each ray
+  vtkm::rendering::raytracing::ChannelBuffer<FloatType> OpticalDepths;
+  vtkm::cont::ArrayHandle<FloatType> PathLengths; // Total distance traversed through the mesh
+};
+}
+}
+} // namespace vtkm::rendering::raytracing
+#endif
