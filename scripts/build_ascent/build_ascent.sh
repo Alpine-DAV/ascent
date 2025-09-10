8ce011691c1b47d5d42dae02fd494c9efa20e5f6
Cyrus Harrison <cyrush@llnl.gov>
2025-09-10 14:28:06 -0700

    kokkos update for el cap

--- scripts/build_ascent/build_ascent.sh
+++ scripts/build_ascent/build_ascent.sh
@@ -581,7 +581,7 @@ fi # build_conduit
 ###############################
 # Kokkos (only for hip or sycl)
 ###############################
-kokkos_version=4.4.01
+kokkos_version=4.7.00
 kokkos_src_dir=$(ospath ${source_dir}/kokkos-${kokkos_version})
 kokkos_build_dir=$(ospath ${build_dir}/kokkos-${kokkos_version})
 kokkos_install_dir=$(ospath ${install_dir}/kokkos-${kokkos_version}/)
@@ -612,17 +612,20 @@ if [[ "$enable_hip" == "ON" ]]; then
   ##
   ## build_ascent specific ROCM_ARCH Map for Kokkos options:
   ##
-  ## TODO: Kokkos 4.5 has MI300A specific option, need to figure out how to
-  ##       map hat in when we update.
+  ## gfx942
   ##
-  ## gfx942 --> Kokkos_ARCH_AMD_GFX942 (MI300A, MI300X)
-  ## (since Kokkos 4.2, since Kokkos 4.5 this should only be used for MI300X)
+  ##  Kokkos 4.5 has MI300A specific option
+  ##    DKokkos_ARCH_AMD_GFX942_APU (MI300A)
+  ##    DKokkos_ARCH_AMD_GFX942 (MI300X)
+  ##
+  ##  We assume MI300A, if we need MI300X, we need a way to differentiate
+  ##  (ROCM_ARCH is gfx942 in both cases)
   ##
   ## gfx90a --> Kokkos_ARCH_AMD_GFX90A (MI200 series)
   ## (since Kokkos 4.2)
   ##
   if [[ "$ROCM_ARCH" == "gfx942" ]]; then
-      kokkos_extra_cmake_args="${kokkos_extra_cmake_args} -DKokkos_ARCH_AMD_GFX942=ON"
+      kokkos_extra_cmake_args="${kokkos_extra_cmake_args} -DKokkos_ARCH_AMD_GFX942_APU=ON"
   fi
 
   if [[ "$ROCM_ARCH" == "gfx90a" ]]; then
