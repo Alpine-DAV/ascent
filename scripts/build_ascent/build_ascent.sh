b2bb01c19e0355ebb79a85950ab5dcd7e47544a2
Cyrus Harrison <cyrush@llnl.gov>
2025-10-21 10:25:12 -0700

    cleanup

--- scripts/build_ascent/build_ascent.sh
+++ scripts/build_ascent/build_ascent.sh
@@ -1071,9 +1071,6 @@ if [[ "$enable_mpicc" == "ON" ]]; then
   echo 'set(MPI_CXX_COMPILER ' ${mpicxx_exe}  ' CACHE PATH "")' >> ${root_dir}/ascent-config.cmake
 fi
 
-echo 'set(BLT_CXX_STD c++17 STRING PATH "")' >> ${root_dir}/ascent-config.cmake
-echo 'set(CMAKE_CXX_STANDARD 17 STRING PATH "")' >> ${root_dir}/ascent-config.cmake
-
 echo 'set(CMAKE_VERBOSE_MAKEFILE ' ${enable_verbose} ' CACHE BOOL "")' >> ${root_dir}/ascent-config.cmake
 echo 'set(CMAKE_BUILD_TYPE ' ${build_config} ' CACHE STRING "")' >> ${root_dir}/ascent-config.cmake
 echo 'set(BUILD_SHARED_LIBS ' ${build_shared_libs} ' CACHE STRING "")' >> ${root_dir}/ascent-config.cmake
@@ -1114,7 +1111,6 @@ fi
 
 if [[ "$enable_hip" == "ON" ]]; then
     echo 'set(ENABLE_HIP ON CACHE BOOL "")' >> ${root_dir}/ascent-config.cmake
-    echo 'set(BLT_CXX_STD c++17 CACHE STRING "")' >> ${root_dir}/ascent-config.cmake
     echo 'set(CMAKE_HIP_COMPILER ' ${CXX} ' CACHE STRING "")' >> ${root_dir}/ascent-config.cmake
     echo 'set(CMAKE_HIP_ARCHITECTURES ' ${ROCM_ARCH} ' CACHE STRING "")' >> ${root_dir}/ascent-config.cmake
     echo 'set(ROCM_PATH ' ${ROCM_PATH} ' CACHE PATH "")' >> ${root_dir}/ascent-config.cmake
