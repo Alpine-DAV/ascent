2fbeec8cb2b4da47c77bfcd7fd8270a8da734c90
Victor Mateevitsi <vmateevitsi@anl.gov>
2025-12-04 19:47:33 +0000

    Removed install_dir

--- scripts/build_ascent/build_ascent_aurora.sh
+++ scripts/build_ascent/build_ascent_aurora.sh
@@ -15,6 +15,4 @@ module load cmake
 module load python/3.10.14
 module load py-cython py-numpy py-pip py-wheel py-setuptools
 
-#env CC=`which icx` CXX=`which icpx` FTN=`which ifx` enable_sycl=ON enable_mpi=ON enable_find_mpi=OFF enable_fortran=ON enable_python=ON raja_enable_vectorization=OFF enable_tests=OFF enable_verbose=OFF install_dir=/soft/visualization/ascent/release/v0.9.4 ./build_ascent_sycl.sh
-env CC=`which mpicc` CXX=`which mpicxx` FTN=`which mpifort` enable_64bit_ids=ON enable_sycl=ON enable_mpi=ON enable_find_mpi=OFF enable_fortran=ON enable_python=ON raja_enable_vectorization=ON enable_tests=OFF enable_verbose=OFF install_dir=/soft/visualization/ascent/release/v0.9.5 ./build_ascent_sycl.sh
-#env CC=`which icx` CXX=`which icpx` FTN=`which ifx` env mpicc_exe=`which mpicc` mpicxx_exe=`which mpicxx` enable_sycl=ON enable_mpi=ON enable_find_mpi=OFF enable_mpicc=ON enable_fortran=ON enable_python=ON raja_enable_vectorization=ON enable_tests=OFF enable_verbose=OFF ./build_ascent_sycl.sh
+env CC=`which mpicc` CXX=`which mpicxx` FTN=`which mpifort` enable_64bit_ids=ON enable_sycl=ON enable_mpi=ON enable_find_mpi=OFF enable_fortran=ON enable_python=ON raja_enable_vectorization=ON enable_tests=OFF enable_verbose=OFF ./build_ascent_sycl.sh
