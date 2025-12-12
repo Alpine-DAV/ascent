59e0cde69f13296b37d83e82d36a01a65d96f223
Nicole Marsaglia <marsaglia1@llnl.gov>
2025-12-12 00:10:54 +0000

    vtkm -> viskores

--- src/tests/ascent/t_ascent_sample.cpp
+++ src/tests/ascent/t_ascent_sample.cpp
@@ -360,10 +360,10 @@ TEST(ascent_sample, box_3d)
 {
     Node n;
     ascent::about(n);
-    // only run this test if ascent was built with vtkm support
-    if(n["runtimes/ascent/vtkm/status"].as_string() == "disabled")
+    // only run this test if ascent was built with viskores support
+    if(n["runtimes/ascent/viskores/status"].as_string() == "disabled")
     {
-        ASCENT_INFO("Ascent vtkm support disabled, skipping test");
+        ASCENT_INFO("Ascent viskores support disabled, skipping test");
         return;
     }
 
