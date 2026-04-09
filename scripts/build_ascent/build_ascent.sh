bcda424a499e3ed0220701687bb2ff39d0661753
Nicole Jo Ann Marsaglia <marsaglia1@llnl.gov>
2026-04-09 12:00:57 -0700

    viskores 1.1.0 patch for annotation occlusion when doing a pseudocolor volume render (i.e. with control points)

--- scripts/build_ascent/build_ascent.sh
+++ scripts/build_ascent/build_ascent.sh
@@ -667,7 +667,7 @@ fi # if enable_hip || enable_sycl
 ################
 # Viskores
 ################
-viskores_version=1.0.0
+viskores_version=1.1.0
 viskores_src_dir=$(ospath ${source_dir}/viskores-${viskores_version})
 viskores_build_dir=$(ospath ${build_dir}/viskores-${viskores_version})
 viskores_install_dir=$(ospath ${install_dir}/viskores-${viskores_version}/)
@@ -687,8 +687,8 @@ if [ ! -d ${viskores_src_dir} ]; then
   # apply patches
   cd ${viskores_src_dir}
   echo "**** Applying Patches to ${viskores_tarball}"
-  patch -p1 < ${script_dir}/2025_06_18_viskores_z_extents_ray_culling_bugfix_viskores_mr109.patch
   patch -p1 < ${script_dir}/2026_01_02_viskores_implent_pan_raytracing.patch
+  patch -p1 < ${script_dir}/2026_04_09_viskores_1_1_0_volume_annotation_occlusion.patch
   cd ${root_dir}
 fi
 
