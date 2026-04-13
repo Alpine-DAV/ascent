095ca7a102340cecf2a0b65db0c395cfbe427aa8
Emily Howell <howell31@llnl.gov>
2026-04-13 14:35:40 -0700

    Task/2 3 2025 json schema for filters (#1663)
    * Save state post resolving conpilation bug :(
    
    * Removing slide verify_params
    
    * Adding some more json schema examples
    
    * Adding a schema validator for flow filters
    
    * Translating all filters in ascent_runtime_vtkh_filters to json schema format. Also adding additional validator constructs needed to complete this.
    
    * Removing temporary helpers and cleaning up files
    
    * Seed should be seeds
    
    * adding a detail namespace to validator
    
    * Converting over the expression filters
    
    * converting another file
    
    * Trying to add some support for expressions again
    
    * Adding a way to pass our expression validator into the flow filter
    
    * We have a valid expression checker!
    
    * Integrating the expression checking into the vtkh number schemas :)
    
    * Taking a pass at trying to get the JIT tests to pass
    
    * Require that either field or topology be given for a threshold filter
    
    * Fixing reflect filter
    
    * Fixing spelling error
    
    * Using refs to nodes instead of copying seperate nodes into tree
    
    * Generalizing the expression checking to any format checking
    
    * Applied formatting fixes
    
    * Removing print statement added for debugging

---
+++ src/libs/flow/flow_schema_validator.hpp
@@ -0,0 +1,57 @@
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+// Copyright (c) Lawrence Livermore National Security, LLC and other Ascent
+// Project developers. See top-level LICENSE AND COPYRIGHT files for dates and
+// other details. No copyright assignment is required to contribute to Ascent.
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+
+//-----------------------------------------------------------------------------
+///
+/// file: flow_schema_validator.hpp
+///
+//-----------------------------------------------------------------------------
+
+#ifndef FLOW_SCHEMA_VALIDATOR_HPP
+#define FLOW_SCHEMA_VALIDATOR_HPP
+
+#include <conduit.hpp>
+
+#include <flow_exports.h>
+#include <flow_config.h>
+
+//-----------------------------------------------------------------------------
+// -- begin flow --
+//-----------------------------------------------------------------------------
+namespace flow
+{
+
+//-----------------------------------------------------------------------------
+// -- begin flow::schema --
+//-----------------------------------------------------------------------------
+namespace schema
+{
+
+using FormatCheckFunction = bool (*)(const std::string &expr, std::string &err_msg);
+
+void FLOW_API register_format_checker(const std::string &format_name, FormatCheckFunction callback);
+
+bool FLOW_API validate(const conduit::Node &schema,
+                       const conduit::Node &input,
+                       conduit::Node &info);
+
+};
+//-----------------------------------------------------------------------------
+// -- end flow::schema --
+//-----------------------------------------------------------------------------
+
+
+//-----------------------------------------------------------------------------
+};
+//-----------------------------------------------------------------------------
+// -- end flow --
+//-----------------------------------------------------------------------------
+
+#endif
+//-----------------------------------------------------------------------------
+// -- end header ifdef guard
+//-----------------------------------------------------------------------------
