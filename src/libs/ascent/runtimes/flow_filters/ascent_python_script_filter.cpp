f65cb3708dddbabdf6223574aed3c64fd410f6f3
Emily Howell <howell31@llnl.gov>
2026-04-14 13:40:04 -0700

    Adding a warning if an "ascent.py" exists in the current working directory

--- src/libs/ascent/runtimes/flow_filters/ascent_python_script_filter.cpp
+++ src/libs/ascent/runtimes/flow_filters/ascent_python_script_filter.cpp
@@ -67,6 +67,14 @@ AscentPythonScript::execute()
         ASCENT_ERROR("AscentPythonScript input must be a DataObject");
     }
 
+
+    if(conduit::utils::is_file("ascent.py"))
+    {
+      ASCENT_WARN("`ascent.py` exists in the current working directory. " <<
+                    "This could cause errors or undesired behavior. " <<
+                    "It is Recomended to remove or rename `ascent.py` and try again.");
+    }
+
     DataObject *data_object = input<DataObject>(0);
 
     conduit::Node *n_input = data_object->as_node().get();
