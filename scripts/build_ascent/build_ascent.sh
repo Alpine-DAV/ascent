2cf570383a56f57a70d5e7755f4947381e19b6f1
Cyrus Harrison <cyrush@llnl.gov>
2026-09-13 10:13:44 -0700

    ...

--- scripts/build_ascent/build_ascent.sh
+++ scripts/build_ascent/build_ascent.sh
@@ -365,7 +365,7 @@ if ${build_pyvenv}; then
     echo "**** Creating Python Virtual Env"
     cd ${install_dir} && ${python_exe} -m venv python-venv
     ${venv_python_exe} -m pip install --upgrade pip
-    ${venv_python_exe} -m pip install numpy sphinx sphinx_rtd_theme wheel setuptools>=61.0.0
+    ${venv_python_exe} -m pip install numpy sphinx sphinx_rtd_theme wheel setuptools
     if ${build_zfp}; then
         ${venv_python_exe} -m pip install cython
     fi
